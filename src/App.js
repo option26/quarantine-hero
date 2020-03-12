4ce929c16ccb7837910bc3770eb842d4e03ed5b3
keno dressel <aeternity@keno.digital>
2020-03-13 00:10:24 +0100

    adds tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-function App() {
+function App () {
+
+  const entrys = [
+    {
+      id: 1,
+      location: 'München',
+      text: 'Brauche jemand, der für mich einkauft',
+      timestamp: Date.now(),
+    }, {
+      id: 2,
+      location: 'München',
+      text: 'Brauche jemand, der für mich einkauft',
+      timestamp: Date.now(),
+    }, {
+      id: 3,
+      location: 'München',
+      text: 'Brauche jemand, der für mich einkauft',
+      timestamp: Date.now(),
+    }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex items-center flex-col">
+      <header className="phone-width">
+        <h1 className="text-6xl text-center">Quarantine Hero</h1>
       </header>
+      <section className="phone-width">
+        <div>
+          <div className="text-3xl mt-4">wir sind menschen. in zeiten der not helfen wir uns. sei ein teil davon.</div>
+        </div>
+        <div className="mt-4">
+          {entrys.map(entry => {
+            return <a className="p-4 border border-gray-400 rounded w-full m-1 text-xl block" href={`/entry/${entry.id}`}>
+              Jemand in <span className="font-bold">{entry.location}</span> bittet um: <span className="italic">{entry.text}</span><br/>
+              <span className="text-gray-500 inline-block text-right w-full text-base">{(new Date(entry.timestamp)).toISOString()}</span>
+            </a>;
+          })}
+        </div>
+      </section>
     </div>
   );
 }
