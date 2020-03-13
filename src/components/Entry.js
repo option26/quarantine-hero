b24c8fbd9b8f2ecb23826cba23a98d1b2a35c157
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-13 14:39:23 +0100

    Add some styling to entries

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,16 +1,29 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import React from 'react';
 import formatDistance from 'date-fns/formatDistance';
+import {de} from 'date-fns/locale';
 
-export default function Entry (props) {
+export default function Entry(props) {
 
-  const date = formatDistance(new Date(props.timestamp), Date.now());
+    const {showFullText, location, id, request, timestamp} = props;
+    const date = formatDistance(new Date(timestamp), Date.now(), {locale: de});
 
-  return (<Link to={`/offer-help/${props.id}`}
-        className="p-4 border border-gray-400 rounded w-full m-1 text-xl block" key={props.id}>
-    Jemand in <span className="font-bold">{props.location}</span> bittet um: <span
-    className="italic">{props.request}</span><br/>
-    <span
-      className="text-gray-500 inline-block text-right w-full text-base">{date} ago</span>
-  </Link>);
+    let textToDisplay;
+
+    if (showFullText) {
+        textToDisplay = request;
+    } else {
+        if (request.length > 300) {
+            textToDisplay = request.substring(0, 300) + "...";
+        } else {
+            textToDisplay = request;
+        }
+    }
+
+    return (<Link to={`/offer-help/${props.id}`}
+                  className="shadow bg-white p-4 border border-gray-400 rounded w-full m-1 mb-5 text-xl block" key={id}>
+        Jemand in <span className="font-bold">{location}</span> braucht Hilfe!
+        <p className="italic mt-3">{textToDisplay}</p><br/>
+        <span className="text-gray-500 inline-block text-right w-full text-base">vor {date}</span>
+    </Link>);
 }
