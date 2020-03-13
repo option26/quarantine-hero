2ca456756e30bccea533f90bab1c407a47de5efb
keno dressel <aeternity@keno.digital>
2020-03-13 15:14:57 +0100

    small visual fixes

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -21,7 +21,7 @@ export default function Entry(props) {
     }
 
     return (<Link to={`/offer-help/${props.id}`}
-                  className="shadow bg-white p-4 border border-gray-400 rounded w-full m-1 mb-5 text-xl block" key={id}>
+                  className="shadow bg-white p-4 border border-gray-400 rounded w-full my-3 text-xl block" key={id}>
         Jemand in <span className="font-bold">{location}</span> braucht Hilfe!
         <p className="italic mt-3">{textToDisplay}</p><br/>
         <span className="text-gray-500 inline-block text-right w-full text-base">vor {date}</span>
