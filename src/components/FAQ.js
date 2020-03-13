828d2b7bb875bc108c82a623d138171259f27159
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-13 21:26:42 +0100

    Add adapted text

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,37 +1,85 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import React from 'react';
 
-export default function FAQ () {
+export default function FAQ() {
 
-  return (<div>
-      <div className="mt-4">
-          <p className="text-2xl font-semibold">Hier werden deine wichtigsten Fragen beantwortet!</p>
-          <p className="text-xl font-semibold mt-2">Wie läuft das ganze ab?</p>
-          <p className="mt-1">Auf quarantänehelden.de findest du Anfragen von Personen, die etwas benötigen. Du kannst dir Anfrangen in deiner Nähe anzeigen lassen und Diese beantworten. Wir leiten deine Nachricht dann an den Anfragensteller weiter der dann zu dir Kontakt aufnimmt. Dann könnt ihr alles weitere wie Details zur Anfrage und Übergabe Zeit und Ort klären.</p>
-          <p className="text-xl font-semibold mt-2">Welche Art von Anfragen kann ich stellen?</p>
-          <p className="mt-1">Prinzipiell kann man alle Anfragen stellen, bei der Hilfe von außen benötigt wird. Das kann ein Einkauf der nötigsten Lebensmittel oder ein Botengang oder irgendetwas anderes sein.</p>
-          <p className="text-xl font-semibold mt-2">Wie bezahlt man für einen Einkauf?</p>
-          <p className="mt-1">Handelt es sich bei der Anfrage um einen Einkauf könnt ihr selbst ausmachen wir ihr die Bezahlung des Einkaufs handhabt. Eine Möglichkeit wäre, das Geld über Paypal zu senden.</p>
-          <p className="text-xl font-semibold mt-2">Wer kann meine Hilfeanfrage sehen?</p>
-          <p className="mt-1">Deine Anfrage und eine ungefährer Ort ist öffentlich einsehbar. Dein Name und deine Emailadresse ist nicht einsehbar.</p>
-          <p className="text-xl font-semibold mt-2">Wie nehmen Helfer Kontakt zu mir auf?</p>
-          <p className="mt-1">Helfer können auf unser Webseite <Link to="/">hier</Link> nach Anfragen in ihrer Nähe suchen. Dann kann man auf die ausgewählte Anfrage klicken und im Formular ein Email an die Person in Quarantäne schicken.</p>
-          <p className="text-xl font-semibold mt-2">Was muss man bei einer Übergabe beachten?</p>
-          <p className="mt-1">Bitte achtet bei einer Übergabe, dass es keine Kontakt zwischen einer Person in Quarantäne und der Helfenden Person gibt. Ihr könnt zum Beispiel etwas vor die Tür legen und dann anrufen, dass es da ist.</p>
-          <p className="text-xl font-semibold mt-2">Werde ich dafür bezahlt?</p>
-          <p className="mt-1">Nein. Das Helfen findet auf freiwilliger Basis statt.</p>
-          <p className="text-xl font-semibold mt-2">Was ist mit meiner Sicherheit?</p>
-          <p className="mt-1">Bitte stelle stets deine eigene Sicherheit an erste Stelle und vermeide direkten Kontakt.</p>
-          <p className="text-xl font-semibold mt-2">Wieso soll ich das tun?</p>
-          <p className="mt-1">Wir sind alle Menschen. Besonders in schwierigen Zeiten müssen wir zusammen halten. Helfe deinen Nachbarn in Quarantäne und ermögliche so eine best mögliche Einschränkung des Viruses.</p>
-          <p className="text-xl font-semibold mt-2">Wie weiß ich, dass die Person wirklich in Quarantäne ist?</p>
-          <p className="mt-1">Unsere Plattform funktioniert nur über die Ehrlichkeit der Nutzer. Wenn du dir unsicher bist, kannst du das dem Anfragesteller mitteilen und zum Beispiel ein Attest erfragen.</p>
-          <p className="text-xl font-semibold mt-2">Wie euch unterstützen?</p>
-          <p className="mt-1">Schau einfach mal ob einer deiner Nachbarn Hilfe benötigt!</p>
-          <p className="text-xl font-semibold mt-2">Wer seid ihr?</p>
-          <p className="mt-1">Wir sind eine Florian, Henrike und Keno, eine Gruppe von Freunden, die mit diesem kleine Projekt Menschen helfen möchten.</p>
+    return (<div className="mb-10">
+            <h1 className="text-2xl font-semibold mt-8">Hier werden deine wichtigsten Fragen beantwortet!</h1>
 
-      </div>
-    </div>
-  );
+            <h2 className="text-xl font-semibold mt-8">Wie funktioniert quarantänehelden.de?</h2>
+
+            <p>Auf der Plattform quarantänehelden.de kommen Menschen zusammen, die sich gegenseitig unterstützen. Hier
+                findest du Anfragen von Personen, die etwas benötigen, aber selbst ihr Zuhause oder ihren Quarantäne-Ort
+                nicht verlassen können. Du kannst dir Anfragen in deiner Nähe anzeigen lassen und diese beantworten. Wir
+                leiten deine Nachricht an den Anfragensteller weiter, der dann zu dir Kontakt aufnimmt. Dann könnt ihr
+                alle
+                weiteren Details zur Anfrage (was wird benötigt, wann und wo?) klären.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Welche Art von Anfragen kann ich stellen?</h2>
+
+            <p>Prinzipiell können alle Anfragen gestellt werden, bei denen Hilfe von außen benötigt wird. Das kann ein
+                Einkauf der nötigsten Lebensmittel, ein Botengang, eine Erledigung oder das Gassigehen mit dem Hund
+                sein.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wie wird für den Einkauf bezahlt?</h2>
+
+            <p>Handelt es sich bei der Anfrage um einen Einkauf, könnt ihr selbst ausmachen, wie ihr die Bezahlung des
+                Einkaufs handhabt. Eine Möglichkeit wäre, das Geld über Paypal zu senden.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wer kann meine Hilfeanfrage sehen?</h2>
+
+            <p>Deine Anfrage und ein ungefährer Ort sind öffentlich einsehbar. Dein Name und deine Emailadresse sind
+                nicht
+                einsehbar.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wie nehmen Helfende Kontakt zu mir auf?</h2>
+
+            <p>Helfende können auf unserer Webseite hier nach Anfragen in ihrer Nähe suchen. Die ausgewählte Anfrage
+                wird
+                angeklickt. Über ein Formular geht eine Email an die Person in Quarantäne. Sie erhält so eine Nachricht
+                von
+                dir und ihr könnt alles weitere verabreden.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Was ist bei einer Übergabe z.B. von Einkäufen zu beachten?</h2>
+
+            <p>Bitte achtet bei einer Übergabe darauf, dass es keinen Kontakt zwischen einer Person in Quarantäne und
+                der
+                helfenden Person gibt. Ihr könnt zum Beispiel etwas vor die Tür legen und dann anrufen, dass es da
+                ist.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Werde ich dafür bezahlt?</h2>
+
+            <p>Nein. Das Helfen findet auf freiwilliger Basis statt.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Was ist mit meiner Sicherheit?</h2>
+
+            <p>Bitte stelle stets deine eigene Sicherheit an erste Stelle und vermeide direkten Kontakt zu Menschen in
+                der
+                Quarantäne.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wieso soll ich das tun?</h2>
+
+            <p>Wir sind eine Gemeinschaft. Besonders in schwierigen Zeiten müssen wir zusammenhalten. Hilf deiner
+                Nachbarschaft in ihrer schwierigen Quarantäne-Situation. Du trägst so auch dazu bei, die Verbreitung
+                des
+                Virus‘ einzuschränken.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wie weiß ich, dass die Person wirklich in Quarantäne ist?</h2>
+
+            <p>Unsere Plattform funktioniert nur über die Ehrlichkeit der Nutzenden. Wenn du dir unsicher
+                bist, kannst du
+                das dem Anfragesteller mitteilen und zum Beispiel ein Attest erfragen.</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wie können wir unterstützen? Da habe ich die Frage nicht ganz verstanden</h2>
+
+            <p>Schau einfach mal, ob eine Person in deinem Umfeld Hilfe benötigt!</p>
+
+            <h2 className="text-xl font-semibold mt-8">Wer seid ihr?</h2>
+
+            <p>Wir sind Florian, Henrike und Keno, eine Gruppe von Freunden, die mit diesem
+                Projekt ihren kleinen Beitrag
+                leisten und Menschen helfen möchten.</p>
+
+        </div>
+    );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,9 @@ export default function Main() {
 
     const getUserData = () => {
         query.get().then(value => {
+
             setEntries(value.docs.map(doc => ({...doc.data().d, id: doc.id})));
+
         });
     };
     useEffect(getUserData, []);
@@ -22,24 +24,38 @@ export default function Main() {
             <div>
                 <div className="w-full flex flex-col justify-center items-center mt-16">
                     <img src={require('../logo.png')}/>
-                    <p className="text-xl py-4">Wir sind Menschen. In Zeiten der Not helfen wir uns. Sei ein Teil
-                        davon.</p>
+                    <p className="text-xl py-4">Wir sind Menschen. In Zeiten der Not helfen wir uns.</p>
                 </div>
-                <p className="text-xl pt-2 pb-2">Viele Menschen befinden sich aktuell freiwillig oder notwendigerweise
-                    in
-                    häuslicher Quarantäne. Wenn ihr diesen Menschen helfen wollt, könnt ihr hier sehen, wobei ihr eure
-                    Mitmenschen in eurer Nachbarschaft unterstützen könnt!<br/><br/>
-                    Wenn ihr gerade in häuslicher Quarantäne seid und Unterstützung bei Einkäufen, Botengängen oder
-                    Gassigehen mit dem Hund benötigt könnt ihr das hier euren Mitmenschen mitteilen!
+                <p className="text-xl pt-2 pb-10">
+
+                    Unsere Plattform vermittelt Helfende an Personen, die Hilfe benötigen.<br/>
+                    <br/>
+                    Viele Menschen befinden sich aktuell freiwillig oder notwendigerweise in häuslicher Quarantäne. Ihr
+                    könnt diesen Menschen helfen! Auf unserer Seite quarantänehelden.de könnt ihr sehen, wie ihr
+                    Mitmenschen in eurem Umfeld unterstützen könnt.<br/>
+                    <br/>
+                    Und wenn ihr euch gerade in häuslicher Quarantäne befindet oder euer Zuhause nicht verlassen könnt,
+                    findet ihr über diese Plattform Unterstützung. Stellt eure Anfrage und findet Helfende. Sie
+                    erledigen für euch Einkäufe, Botengänge oder gehen mit eurem Hund Gassi.<br/>
+                    <br/>
+                    Das geht ganz einfach. Wer Hilfe benötigt, stellt eine Anfrage. Wer helfen möchte, guckt sich die
+                    Anfragen in seiner Nähe an und kann entscheiden, wo er/sie aktiv werden kann. Die Kontaktaufnahme
+                    läuft dann über diese Plattform.<br/>
+                    <br/>
+                    Tragt diese Infos bitte auch an andere weiter, die gerne helfe wollen oder die Hilfe von außen
+                    benötigen!
+                    <br/>
                 </p>
                 <div className="flex justify-between">
                     <Link to="/overview"
 
-                          className="font-bold py-8 px-4 rounded bg-primary text-center text-white flex-1 mr-4">Ich möchte helfen
+                          className="font-bold py-8 px-4 rounded bg-primary text-center text-white flex-1 mr-4">Ich
+                        möchte helfen
                     </Link>
                     <Link to="/signup"
 
-                          className="font-bold py-8 px-4 rounded bg-primary text-center text-white flex-1">Ich brauche Hilfe
+                          className="font-bold py-8 px-4 rounded bg-primary text-center text-white flex-1">Ich brauche
+                        Hilfe
                     </Link>
                 </div>
                 {entries.map(entry => (<Entry {...entry} key={entry.id}/>))}
