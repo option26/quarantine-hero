f70f92e58570d4c6d27e0bf310367fab13b1fab5
keno dressel <aeternity@keno.digital>
2020-03-13 23:31:54 +0100

    adds dsgvo

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseApp from 'firebase';
 import SuccessOffer from './components/SuccessOffer';
+import DSGVO from './components/DSGVO';
 import CookieConsent from 'react-cookie-consent';
 
 import {
@@ -79,6 +80,9 @@ function App (props) {
             </Route>
             <Route path="/success-offer">
               <SuccessOffer/>
+            </Route>
+            <Route path="/dsgvo">
+              <DSGVO/>
             </Route>
             <Route path="/">
               <Main/>
