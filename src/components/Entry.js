40c585b663c6e36189e0d5be965676637d85df40
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-13 14:20:03 +0100

    Add relative time formatting

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import formatDistance from 'date-fns/formatDistance';
 
 export default function Entry (props) {
+
+  const date = formatDistance(new Date(props.timestamp), Date.now());
+
   return (<Link to={`/offer-help/${props.id}`}
         className="p-4 border border-gray-400 rounded w-full m-1 text-xl block" key={props.id}>
     Jemand in <span className="font-bold">{props.location}</span> bittet um: <span
     className="italic">{props.request}</span><br/>
     <span
-      className="text-gray-500 inline-block text-right w-full text-base">{(new Date(props.timestamp)).toISOString()}</span>
+      className="text-gray-500 inline-block text-right w-full text-base">{date} ago</span>
   </Link>);
 }
