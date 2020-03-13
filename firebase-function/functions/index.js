c13efcf1fc0c34c67074a15a8867000c9ce6675b
keno dressel <aeternity@keno.digital>
2020-03-13 15:53:17 +0100

    adds proper emails to sendgrid function

--- firebase-function/functions/index.js
+++ firebase-function/functions/index.js
@@ -7,34 +7,28 @@ sgMail.setApiKey(
 );
 
 exports.firestoreRequest = functions.region('europe-west1').firestore.document('/ask-for-help/{requestId}/offer-help/{offerId}')
-  .onCreate((snap, context) => {
+  .onCreate(async (snap, context) => {
 
     const parentPath = snap.ref.parent.path; // get the id
     const offerId = snap.id; // get the id
     const db = admin.firestore();
+    const askForHelp = snap.ref.parent.parent;
 
-    return db.collection(parentPath).doc(offerId)
-      .get()
-      .then(doc => {
-        const { answer, email } = doc.data();
-        const text = `<div>
-      <h4>Information</h4>
-      <ul>
-        <li>
-          Email - ${email || ''}
-        </li>
-      </ul>
-      <h4>Message</h4>
-      <p>${answer || ''}</p>
-    </div>`;
-        const msg = {
-          to: 'ardobras@googlemail.com',
-          from: 'no-reply@myemail.com',
-          subject: `QuarantäneHelden - Jemand hat dir geschrieben!`,
-          text: text,
-          html: text,
-        };
-
-        sgMail.send(msg);
-      });
+    const offer = await db.collection(parentPath).doc(offerId).get();
+    const askRecord = await askForHelp.get();
+    const { request, uid } = askRecord.data().d;
+    const data = await admin.auth().getUser(uid);
+    const { email: receiver } = data.toJSON();
+    const { answer, email } = offer.data();
+    return await sgMail.send({
+      to: receiver,
+      from: email,
+      templateId: 'd-ed9746e4ff064676b7df121c81037fab',
+      dynamic_template_data: {
+        subject: 'QuarantäneHelden - Jemand hat dir geschrieben!',
+        answer,
+        email,
+        request
+      }
+    });
   });
