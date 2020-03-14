ebd844f297393d99ec19741ad121f2925bfda6bd
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-14 17:47:03 +0100

    Add success image also to success-offer page

--- src/components/SuccessOffer.js
+++ src/components/SuccessOffer.js
@@ -5,12 +5,11 @@ import Footer from './Footer';
 
 export default function SuccessOffer () {
 
-  return (<div>
-      <div className="mt-4 p-1">
-        <p className="text-2xl font-teaser mb-8 text-center">Vielen Dank, wir haben Deine Nachricht weitergeleitet.</p>
-        <div className="flex justify-center items-center mb-8">
-          <Link className="btn-green" to={'/'}>ZUR STARTSEITE</Link>
-        </div>
+  return (<div className="mt-8 p-1">
+      <p className="text-2xl font-teaser mb-8 text-center">Vielen Dank, wir haben Deine Nachricht weitergeleitet.</p>
+      <div className="flex justify-center flex-col items-center mb-8">
+        <img className="h-48 w-48 my-10" src={require('../assets/success.svg')} alt=""/>
+        <Link className="btn-green mt-10" to={'/'}>ZUR STARTSEITE</Link>
       </div>
       <Footer />
     </div>
