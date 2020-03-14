ff61c02fc30514992d90ab6634d4653bd248e989
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-14 13:31:31 +0100

    Add passwort reset flow

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,6 +5,7 @@ import 'firebase/auth';
 import firebaseApp from 'firebase';
 import {Redirect} from "react-router-dom";
 import Footer from './Footer';
+import fb from '../firebase';
 
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
@@ -16,6 +17,9 @@ const Signup = (props) => {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [error, setError] = React.useState('');
+    const [passwordResetSuccess, setPasswordResetSuccess] = React.useState(false);
+    const [passwordResetError, setPasswordResetError] = React.useState(false);
+
     const {
         user,
         signInWithEmailAndPassword,
@@ -27,48 +31,64 @@ const Signup = (props) => {
     }
 
     const signInOrRegister = async e => {
-      console.log("ell")
-      e.preventDefault();
-      let result = await createUserWithEmailAndPassword(email, password);
-      if(result.code === 'auth/email-already-in-use') result = await signInWithEmailAndPassword(email, password);
-      if(result.code) setError(result.message);
+        console.log("ell")
+        e.preventDefault();
+        let result = await createUserWithEmailAndPassword(email, password);
+        if (result.code === 'auth/email-already-in-use') result = await signInWithEmailAndPassword(email, password);
+        if (result.code) setError(result.message);
     };
 
-    return <form className="p-4 mt-8" onSubmit={signInOrRegister}>
-        <div className="mb-4">
-          <div className="font-teaser mb-6">
-            Registriere dich mit deiner E-Mail und einem Passwort um eine Hilfe-Anfrage zu posten.
-          </div>
-            <label className="block text-gray-700 text-sm font-bold mb-1 font-open-sans" htmlFor="username">
-                Email
-            </label>
-            <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none input-focus"
-                id="username" type="email" placeholder="Deine E-Mailadresse" value={email} required="required"
-                onChange={e => setEmail(e.target.value)}/>
-        </div>
-        <div className="mb-8">
-            <label className="block text-gray-700 text-sm font-bold mb-1 text font-open-sans" htmlFor="password">
-                Passwort
-            </label>
-            <input
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none input-focus"
-                id="password" type="password" placeholder="Dein Passwort" value={password} required="required"
-                onChange={e => setPassword(e.target.value)}/>
-        </div>
-      {error ? <div className="text-red-500">
-        {error}
-      </div> : ''}
-        <div className="flex justify-end my-6">
-            <button
-                className="btn-green w-full"
-                type="submit">
-                Jetzt Registrieren
-            </button>
-        </div>
-
-          <Footer />
-    </form>;
+    return <div className="p-4 mt-8">
+        <form onSubmit={signInOrRegister}>
+            <div className="mb-4">
+                <div className="font-teaser mb-6">
+                    Registriere dich mit deiner E-Mail und einem Passwort um eine Hilfe-Anfrage zu posten oder melde
+                    dich
+                    an, wenn du bereits einen Account besitzt.
+                </div>
+                <label className="block text-gray-700 text-sm font-bold mb-1 font-open-sans" htmlFor="username">
+                    Email
+                </label>
+                <input
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none input-focus"
+                    id="username" type="email" placeholder="Deine E-Mailadresse" value={email} required="required"
+                    onChange={e => setEmail(e.target.value)}/>
+            </div>
+            <div className="mb-8">
+                <label className="block text-gray-700 text-sm font-bold mb-1 text font-open-sans" htmlFor="password">
+                    Passwort
+                </label>
+                <input
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none input-focus"
+                    id="password" type="password" placeholder="Dein Passwort" value={password} required="required"
+                    onChange={e => setPassword(e.target.value)}/>
+            </div>
+            {error ? <div className="text-red-500">
+                {error}
+            </div> : ''}
+            <div className="flex justify-end my-6">
+                <button
+                    className="btn-green w-full"
+                    type="submit">
+                    Jetzt Registrieren oder Anmelden
+                </button>
+            </div>
+        </form>
+        <button onClick={(e) => {
+            e.preventDefault();
+            fb.auth.sendPasswordResetEmail(email, {
+                url: 'https://www.quarantaenehelden.org/#/signup',
+                handleCodeInApp: false
+            })
+                .then(() => setPasswordResetSuccess(true))
+                .catch((err) => setPasswordResetError(true));
+        }}
+                className="btn-green w-full">
+            Passwort zurücksetzen
+        </button>
+        {passwordResetSuccess && <div className="my-5 bg-yellow-100 border rounded p-2 px-4 text-gray-800">Eine Email mit Anleitung zum Zurücksetzen deines Passworts wurde dir zugesendet!</div>}
+        <Footer/>
+    </div>;
 }
 
 export default withFirebaseAuth({
