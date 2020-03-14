1e3fd0ce48a9db4c9479d5265242aa05dbc4acdb
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-14 16:57:07 +0100

    Add a nice picture to the success screen

--- src/components/Success.js
+++ src/components/Success.js
@@ -6,9 +6,10 @@ import Footer from './Footer';
 export default function Success () {
 
   return (<div>
-      <div className="mt-4 p-1">
+      <div className="mt-8 p-1">
         <p className="text-2xl font-teaser mb-8 text-center">Vielen Dank, wir haben deine Anfrage eingestellt.</p>
-        <div className="flex justify-center items-center mb-8">
+        <div className="flex justify-center flex-col items-center mb-8">
+          <img className="h-48 w-48 my-10" src={require('../assets/success.svg')}></img>
           <Link className="btn-green" to={'/dashboard'}>ZU DEINER ÜBERSICHT</Link>
         </div>
       </div>
