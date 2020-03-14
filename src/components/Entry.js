3e1647d39efa0e1ed237ec20671cfe8aea4467d1
keno dressel <aeternity@keno.digital>
2020-03-14 11:49:07 +0100

    moves time futher down on entry

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -10,7 +10,7 @@ export default function Entry(props) {
     const date = formatDistance(new Date(timestamp), Date.now(), {locale: de});
 
     let textToDisplay;
-    const [deleted, setDeleted] = useState('')
+    const [deleted, setDeleted] = useState('');
 
     if (showFullText) {
         textToDisplay = request;
@@ -29,8 +29,8 @@ export default function Entry(props) {
     };
 
     return (deleted ? false : <Link to={`/offer-help/${props.id}`}
-                  className="shadow bg-white p-4 rounded w-full my-3 text-xl block" key={id}>
-      <span className="text-xs font-open-sans text-gray-800">Jemand in <span className="font-bold">{location}</span> braucht Hilfe!</span>
+                  className="shadow bg-white px-4 py-2 rounded w-full my-3 text-xl block" key={id}>
+      <span className="text-xs font-open-sans text-gray-800 mt-2">Jemand in <span className="font-bold">{location}</span> braucht Hilfe!</span>
         <p className="mt-2 mb-2 font-open-sans text-gray-800">{textToDisplay}</p>
         <span className="text-gray-500 inline-block text-right w-full text-xs font-open-sans">vor {date}</span>
       {fb.auth.currentUser && fb.auth.currentUser.uid  === props.uid ? <div>
