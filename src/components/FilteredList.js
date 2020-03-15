c45cb6435cc19e2b2a75e8c105227598e124cb73
keno dressel <aeternity@keno.digital>
2020-03-15 15:10:08 +0100

    bars google maps behind feature flag

--- src/components/FilteredList.js
+++ src/components/FilteredList.js
@@ -4,13 +4,19 @@ import { GeoFirestore } from 'geofirestore';
 import { getLatLng, geocodeByAddress } from 'react-places-autocomplete';
 import Entry from './Entry';
 import LocationInput from './LocationInput';
+import { isMapsApiEnabled } from '../featureFlags.js';
 
 export default function FilteredList () {
 
   const [location, setLocation] = useState('');
-  const [entries, setEntries] = useState([{
-    id: "placeholder-id"
-  }]);
+  const [entries, setEntries] = useState([
+    {
+      id: 'placeholder-id',
+    }]);
+  const [filteredEntries, setFilteredEntries] = useState([
+    {
+      id: 'placeholder-id',
+    }]);
   const [searchCompleted, setSearchCompleted] = useState(false);
 
   const collection = fb.store.collection('ask-for-help');
@@ -19,6 +25,7 @@ export default function FilteredList () {
   const getUserData = () => {
     query.get().then(value => {
       setEntries(value.docs.map(doc => ({ ...doc.data().d, id: doc.id })));
+      setFilteredEntries(value.docs.map(doc => ({ ...doc.data().d, id: doc.id })));
     });
   };
 
@@ -30,30 +37,39 @@ export default function FilteredList () {
 // Create a GeoCollection reference
   const geocollection = geofirestore.collection('ask-for-help');
 
+  const handleChange = e => {
+    setLocation(e.target.value);
+    if (!isMapsApiEnabled) {
+      setFilteredEntries(entries.filter(entry => String(entry.plz).indexOf(String(e.target.value).slice(0,2)) === 0));
+    }
+  };
+
   const handleSelect = address => {
     setLocation(address);
-    geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(coordinates =>  {
-        const query = geocollection.near({ center: new fb.app.firestore.GeoPoint(coordinates.lat, coordinates.lng), radius: 30 });
-        query.get().then((value) => {
-          // All GeoDocument returned by GeoQuery, like the GeoDocument added above
-          console.log(value.docs)
-          setEntries(value.docs.map(doc => ({...doc.data(), id: doc.id})));
-          setSearchCompleted(true);
-        });
-      })
-      .catch(error => console.error('Error', error));
+    if (isMapsApiEnabled) {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(coordinates => {
+          const query = geocollection.near({ center: new fb.app.firestore.GeoPoint(coordinates.lat, coordinates.lng), radius: 30 });
+          query.get().then((value) => {
+            // All GeoDocument returned by GeoQuery, like the GeoDocument added above
+            setEntries(value.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+            setSearchCompleted(true);
+          });
+        })
+        .catch(error => console.error('Error', error));
+    }
   };
 
   return (<div>
       <div className="py-3">
-        <LocationInput onChange={setLocation} value={location} onSelect={handleSelect} />
+        <LocationInput onChange={handleChange} value={location} onSelect={handleSelect}/>
       </div>
       <div className="py-3 w-full">
         {entries.length === 0 ? (!searchCompleted || location.length === 0 ?
           <span>Bitte gib deinen Standort ein.</span> :
-          <span className="my-5 font-open-sans text-gray-800">In {location} gibt es aktuell keine Anfragen.</span>) : entries.map(entry => (<Entry key={entry.id} {...entry}/>))}
+          <span className="my-5 font-open-sans text-gray-800">In {location} gibt es aktuell keine Anfragen.</span>) : filteredEntries.map(
+          entry => (<Entry key={entry.id} {...entry}/>))}
       </div>
     </div>
   );
