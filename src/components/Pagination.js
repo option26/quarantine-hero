1b1571bc28c7d18e596903a7c8371ef1a2676052
Maurice <maurice.ackel@mk29.de>
2020-03-18 11:34:38 +0100

    Now includes a button to get to the most recent posts.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -8,9 +8,9 @@ export default function Pagination(props) {
       <button onClick={e=> props.onPrevPage(e.target.value)} class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
         Zurück
       </button>
-      {/* {
-       [1,2,3].map(n => <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">n</button>)
-      } */}
+      <button onClick={e=> props.onShowCurrent(e.target.value)} class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4">
+        Aktuellste
+      </button>
       <button onClick={e=> props.onNextPage(e.target.value)} class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
         Weiter
       </button>
