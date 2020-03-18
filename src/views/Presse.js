e2a327af6cfd04cd42e73a87652f6719e1954f25
keno dressel <aeternity@keno.digital>
2020-03-18 10:55:32 +0100

    adds images to presse

--- src/views/Presse.js
+++ src/views/Presse.js
@@ -5,7 +5,7 @@ export default function Presse () {
 
   const Article = (props) => {
     return (
-      <div className="bg-kaki p-4 mb-6 mt-4 font-open-sans w-full">
+      <div className="bg-kaki p-4 mb-4 mt-4 font-open-sans w-full">
         <div className="text-xs text-gray-700">{props.date}</div>
         <div className="font-bold">{props.title}</div>
         <a className="text-secondary w-full block truncate" href={props.link} target="_blank" rel="nofollow noopener noreferrer">{props.link}</a>
@@ -14,11 +14,31 @@ export default function Presse () {
   };
 
   const articles = [
-    /* {
-      date: '17.03.2020',
-      title: 'BUNTE - Menschlichkeit in Corona Zeiten, junge Menschen helfen.',
-      link: 'https://www.bunte.de/asdfgasewrgfaerghasergasergasergaserg'
-    } */
+    {
+      date: '16.03.2020',
+      title: 'rpr1 - RPR1.Corona-Hilfenetzwerk',
+      link: 'https://www.rpr1.de/programm/aktion/rpr1corona-hilfenetzwerk'
+    },
+    {
+      date: '16.03.2020',
+      title: 'BUNTE.de - Quarantänehelden: So kannst du Menschen in Quarantäne jetzt helfen',
+      link: 'https://www.bunte.de/family/bewegende-geschichten/geschichten-des-lebens/gegenseitige-unterstuetzung-organisation-quarantaene-helden-hier-hilft-man-sich-waehrend-der-corona.html'
+    },
+    {
+      date: '16.03.2020',
+      title: 'businessinsider.de - So könnt ihr andere Menschen in Zeiten von Corona unterstützen — oder selbst Hilfe bekommen',
+      link: 'https://www.businessinsider.de/wirtschaft/verbraucher/so-koennt-ihr-andere-menschen-in-zeiten-von-corona-unterstuetzen-oder-selbst-hilfe-bekommen/'
+    },
+    {
+      date: '16.03.2020',
+      title: 'Sat1 Regional - #NachbarschaftsChallenge: Wie Menschen in der Coronavirus-Krise auf andere Weise zusammenrücken',
+      link: 'https://www.sat1regional.de/nachbarschaftschallenge-wie-menschen-in-der-coronavirus-krise-zusammenruecken/'
+    },
+     {
+      date: '16.03.2020',
+      title: 'Utopia.de - Quarantänehelden: So kannst du Menschen in Quarantäne jetzt helfen',
+      link: 'https://utopia.de/quarantaenehelden-corona-virus-helfen-179241/'
+    }
   ];
 
   return (<div>
@@ -42,6 +62,26 @@ export default function Presse () {
             <div>
               <a href='https://we.tl/t-FhgDBEFBih' className="text-secondary hover:underline" target="_blank" rel="noopener noreferrer">QuarantaeneHelden.zip</a>
             </div>
+          </div>
+        </div>
+        <div className="my-8 flex flex-wrap">
+          <div className="w-1/3 h-auto" >
+            <img src={require('../assets/bunte_de.jpg')} />
+          </div>
+          <div className="w-1/3 h-auto" >
+            <img src={require('../assets/business_insider.jpg')} />
+          </div>
+          <div className="w-1/3 h-auto" >
+            <img src={require('../assets/rpr1.jpg')} />
+          </div>
+          <div className="w-1/3 h-auto" >
+            <img src={require('../assets/sat1_regional.jpg')} />
+          </div>
+          <div className="w-1/3 h-auto" >
+            <img src={require('../assets/utopia.jpg')} />
+          </div>
+          <div className="w-1/3 h-auto" >
+            <img src={require('../assets/youfm.jpg')} />
           </div>
         </div>
         {articles.map(article => <Article {...article} key={article.link} />)}
