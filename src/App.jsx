b9d98b03e948a2b2d4d8c5b3dd224096a22af470
Florian Schmidt <florian.schmidt.1994@icloud.com>
2020-03-20 01:25:37 +0100

    Additional linting fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import './styles/App.css';
-import Main from './views/Main.js';
-import OfferHelp from './views/OfferHelp.js';
-import Dashboard from './views/Dashboard.js';
-import FAQ from './views/FAQ.js';
-import Impressum from './views/Impressum.js';
-import Signup from './views/Signup';
-import AskForHelp from './views/AskForHelp';
-import Overview from './views/Overview';
-import Success from './views/Success';
 import withFirebaseAuth from 'react-with-firebase-auth';
-import fb from './firebase';
-import SuccessOffer from './views/SuccessOffer';
-import DSGVO from './views/DSGVO';
 import CookieConsent from 'react-cookie-consent';
-import ScrollUpButton from "react-scroll-up-button";
+import ScrollUpButton from 'react-scroll-up-button';
 import MenuIcon from '@material-ui/icons/Menu';
 import {
   HashRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
 } from 'react-router-dom';
-import Sidebar from "./components/Sidebar";
+import Main from './views/Main';
+import OfferHelp from './views/OfferHelp';
+import Dashboard from './views/Dashboard';
+import FAQ from './views/FAQ';
+import Impressum from './views/Impressum';
+import Signup from './views/Signup';
+import AskForHelp from './views/AskForHelp';
+import Overview from './views/Overview';
+import Success from './views/Success';
+import fb from './firebase';
+import SuccessOffer from './views/SuccessOffer';
+import DSGVO from './views/DSGVO';
+import Sidebar from './components/Sidebar';
 import DesktopMenu from './components/DesktopMenu';
-import VerifyEmail from "./views/VerifyEmail";
-import CompleteOfferHelp from "./views/CompleteOfferHelp";
+import VerifyEmail from './views/VerifyEmail';
+import CompleteOfferHelp from './views/CompleteOfferHelp';
 import NotifyMe from './views/NotifyMe';
-import ScrollToTop from "./components/ScrollToTop";
-import ShareButtons from "./components/ShareButtons";
+import ScrollToTop from './components/ScrollToTop';
+import ShareButtons from './components/ShareButtons';
 import Presse from './views/Presse';
 
-function App (props) {
+function App(props) {
   const {
     user,
     signOut,
@@ -46,11 +46,11 @@ function App (props) {
     };
     handleHashChange();
 
-    window.addEventListener("hashchange", handleHashChange);
+    window.addEventListener('hashchange', handleHashChange);
   };
 
   useEffect(() => {
-    if(document.cookie.indexOf('cookieConsent') > -1) addListener();
+    if (document.cookie.indexOf('cookieConsent') > -1) addListener();
   }, []);
 
   const [menuOpen, setMenuOpen] = useState(false);
@@ -58,62 +58,63 @@ function App (props) {
   return (
     <div className="flex items-center min-h-screen flex-col bg-kaki">
       <Router>
-      <div className="hidden md:flex justify-end md:mt-12 w-full phone-width items-center">
-        <Link className="mr-4 font-open-sans text-gray-700" to={`/presse`}>Presse</Link><ShareButtons />
-      </div>
-      <div className="phone-width bg-white shadow-xl min-h-screen md:mt-6">
-          <ScrollToTop/>
+        <div className="hidden md:flex justify-end md:mt-12 w-full phone-width items-center">
+          <Link className="mr-4 font-open-sans text-gray-700" to="/presse">Presse</Link>
+          <ShareButtons />
+        </div>
+        <div className="phone-width bg-white shadow-xl min-h-screen md:mt-6">
+          <ScrollToTop />
           <DesktopMenu isLoggedIn={user} signOut={signOut} />
           <div className="md:px-16 overflow-hidden">
-            <div style={{zIndex: 101}} className="visible md:invisible h-16 w-full fixed top-0 bg-white flex flex-row justify-between w-full items-center pr-5">
-              <Link to="/" className="font-main ml-4" style={{fontWeight: '600'}}>
+            <div style={{ zIndex: 101 }} className="visible md:invisible h-16 w-full fixed top-0 bg-white flex flex-row justify-between w-full items-center pr-5">
+              <Link to="/" className="font-main ml-4" style={{ fontWeight: '600' }}>
                 <img alt="logo" src={require('./assets/logo_invert.svg')} className="h-10" />
               </Link>
               <div>
-                <MenuIcon style={{ fontSize: '40px' }} className="text-gray-600" onClick={() => setMenuOpen(true)}/>
+                <MenuIcon style={{ fontSize: '40px' }} className="text-gray-600" onClick={() => setMenuOpen(true)} />
               </div>
             </div>
-            <Sidebar open={menuOpen} onClose={() => setMenuOpen(false)} isLoggedIn={user} signOut={signOut}/>
+            <Sidebar open={menuOpen} onClose={() => setMenuOpen(false)} isLoggedIn={user} signOut={signOut} />
             <div className="mt-20 md:mt-0">
-              <Switch >
+              <Switch>
                 <Route path="/offer-help/:id">
-                  <OfferHelp/>
+                  <OfferHelp />
                 </Route>
                 <Route path="/signup/:returnUrl">
-                  <Signup/>
+                  <Signup />
                 </Route>
                 <Route path="/signup">
-                  <Signup/>
+                  <Signup />
                 </Route>
                 <Route path="/verify-email">
-                  <VerifyEmail/>
+                  <VerifyEmail />
                 </Route>
                 <Route path="/ask-for-help">
-                  <AskForHelp/>
+                  <AskForHelp />
                 </Route>
                 <Route path="/dashboard">
-                  <Dashboard/>
+                  <Dashboard />
                 </Route>
                 <Route path="/faq">
-                  <FAQ/>
+                  <FAQ />
                 </Route>
                 <Route path="/impressum">
-                  <Impressum/>
+                  <Impressum />
                 </Route>
                 <Route path="/overview">
-                  <Overview/>
+                  <Overview />
                 </Route>
                 <Route path="/success">
-                  <Success/>
+                  <Success />
                 </Route>
                 <Route path="/success-offer">
-                  <SuccessOffer/>
+                  <SuccessOffer />
                 </Route>
                 <Route path="/dsgvo">
-                  <DSGVO/>
+                  <DSGVO />
                 </Route>
                 <Route path="/presse">
-                  <Presse/>
+                  <Presse />
                 </Route>
                 <Route path="/notify-me">
                   <NotifyMe />
@@ -122,13 +123,13 @@ function App (props) {
                   <CompleteOfferHelp />
                 </Route>
                 <Route path="/">
-                  <Main/>
+                  <Main />
                 </Route>
               </Switch>
-              <ScrollUpButton/>
+              <ScrollUpButton />
             </div>
           </div>
-      </div>
+        </div>
       </Router>
       <CookieConsent
         location="bottom"
@@ -137,7 +138,8 @@ function App (props) {
         style={{ background: '#2B373B' }}
         buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
         onAccept={addListener}
-        expires={365}>
+        expires={365}
+      >
         Diese Webseite verwendet Cookies, um das Nutzererlebnis zu verbessern.
       </CookieConsent>
     </div>
