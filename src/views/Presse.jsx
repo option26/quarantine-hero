9f4a0752bc90c05538970dcb97d66682ab6fecfd
Maurice22 <m.ackel@icloud.com>
2020-03-20 15:11:18 +0100

    Removed rounded property

--- src/views/Presse.jsx
+++ src/views/Presse.jsx
@@ -48,7 +48,7 @@ export default function Presse() {
           Neben vielen lokalen Helden haben unter anderem diese Medien geholfen die QuarantäneHelden bekannt zu machen um noch mehr Hilfesuchende zu unterstützen.
           Hier findet ihr aktuelle Artikel und Nachrichten.
         </div>
-        <div className="bg-kaki p-4 mb-10 mt-8 font-open-sans flex items-center rounded">
+        <div className="bg-kaki p-4 mb-10 mt-8 font-open-sans flex">
           <div>
             <img src={require('../assets/clipboard.svg')} alt="Clipboard" className="w-12 mr-4" />
           </div>
