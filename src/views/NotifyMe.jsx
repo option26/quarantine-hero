f7ba7e874b2e038c090313040614453ff7764cbd
yorickreum <webdesign@yorickreum.de>
2020-03-23 01:00:49 +0100

    i18n for NotifyMe view

--- src/views/NotifyMe.jsx
+++ src/views/NotifyMe.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import fb from '../firebase';
 import LocationInput from '../components/LocationInput';
 import Footer from '../components/Footer';
 import { baseUrl } from '../appConfig';
 
 export default function NotifyMe() {
+  const { t } = useTranslation();
+
   const [email, setEmail] = useState('');
   const [signInLinkSent, setSignInLinkSent] = useState(false);
   const [location, setLocation] = useState('');
@@ -37,44 +40,39 @@ export default function NotifyMe() {
     return (
       <div className="p-4">
         <div className="font-teaser my-6">
-          Lass Dich benachrichtigen, wenn Jemand in Deiner Umgebung Hilfe braucht.
+          {t('views.notifyMe.beNotified')}
         </div>
         <div className="border bg-secondary px-4 py-2 rounded text-white flex flex-row items-center border">
-          Wir haben dir eine Email gesendet! Bitte überprüfe dein Postfach und klicke auf den Link in unserer Email! Wir
-          werden dich dann benachrichtigen, wenn Leute in
-          {' '}
-          {location}
-          {' '}
-          Hilfe benötigen.
+          {t('views.notifyMe.emailSent', { location })}
         </div>
       </div>
 
     );
   }
+
   return (
     <div className="p-4">
       <div className="font-teaser my-6">
-        Lass Dich benachrichtigen, wenn Jemand in Deiner Umgebung Hilfe braucht.
+        {t('views.notifyMe.beNotified')}
       </div>
       <form onSubmit={handleSubmit}>
-        <LocationInput required onChange={handleChange} value={location} onSelect={handleSelect} />
+        <LocationInput required onChange={handleChange} value={location} onSelect={handleSelect}/>
         <input
           className="input-focus my-6"
           type="email"
-          placeholder="Deine Emailadresse"
+          placeholder={t('views.notifyMe.yourMail')}
           onChange={(e) => setEmail(e.target.value)}
           value={email}
           required
         />
         <button className="mt-6 btn-green w-full disabled:opacity-75 disabled:cursor-not-allowed" type="submit">
-          Benachrichtige mich wenn jemand in
-          {' '}
-          {location && location !== '' ? `der Nähe von ${location}` : 'meiner Nähe'}
-          {' '}
-          Hilfe braucht!
+          {t(
+            'views.notifyMe.notifyMe',
+            { location: (location && location !== '' ? `der Nähe von ${location}` : 'meiner Nähe') },
+          )}
         </button>
       </form>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
