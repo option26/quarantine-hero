d14c6895c9ded5b1e47a6937703ebf8ff158e512
Maurice22 <m.ackel@icloud.com>
2020-03-27 21:19:01 +0100

    Added password confirm input and custom validation if passwords don't match

--- src/views/Signup.jsx
+++ src/views/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import withFirebaseAuth from 'react-with-firebase-auth';
 import * as firebaseApp from 'firebase/app';
 import 'firebase/auth';
@@ -17,6 +17,8 @@ const Signup = (props) => {
   const [password, setPassword] = React.useState('');
   const [error, setError] = React.useState('');
   const location = useLocation();
+  const passwordInput = useRef();
+  const passwordRepeatInput = useRef();
 
   const {
     user,
@@ -50,6 +52,14 @@ const Signup = (props) => {
     if (!signUpResult.code) await signUpResult.user.sendEmailVerification();
   };
 
+  const comparePasswords = () => {
+    if (passwordInput.current.value !== passwordRepeatInput.current.value) {
+      passwordRepeatInput.current.setCustomValidity('Die Passwörter stimmen nicht überein');
+    } else {
+      passwordRepeatInput.current.setCustomValidity('');
+    }
+  };
+
   return (
     <div className="p-4 mt-8">
       <form onSubmit={registerUser}>
@@ -62,18 +72,36 @@ const Signup = (props) => {
           </label>
           <MailInput className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none input-focus" placeholder="Deine Emailadresse" onChange={setEmail} defaultValue={email} />
         </div>
-        <div className="mb-8">
+        <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-1 text font-open-sans" htmlFor="password">
             Passwort
           </label>
           <input
+            ref={passwordInput}
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none input-focus"
             id="password"
             type="password"
             placeholder="Dein Passwort"
             value={password}
             required="required"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              comparePasswords();
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-8">
+          <label className="block text-gray-700 text-sm font-bold mb-1 text font-open-sans" htmlFor="password">
+            Passwort wiederholen
+          </label>
+          <input
+            ref={passwordRepeatInput}
+            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none input-focus"
+            id="password_repeat"
+            type="password"
+            placeholder="Password wiederholen"
+            required="required"
+            onChange={comparePasswords}
           />
         </div>
         {error ? (
