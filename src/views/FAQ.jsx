bed87e40c78da06882e44447ed984c74823393f9
Tom Graupner <super-git@tomgraupner.de>
2020-03-30 23:40:54 +0200

    Updated FAQ component with a new logic and new content

--- src/views/FAQ.jsx
+++ src/views/FAQ.jsx
@@ -1,63 +1,74 @@
+/* eslint-disable no-else-return */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Footer from '../components/Footer';
 
-const QA = (props) => (
-  <>
-    <h2 className="text-xl font-teaser mt-8">{props.question}</h2>
-    <p className="font-open-sans">{props.children}</p>
-  </>
-);
-
 export default function FAQ() {
-  const { t } = useTranslation();
-
-  const faqTranslationStringHowTo = [
+  const { t, i18n } = useTranslation();
+  const faqTranslationStrings = [
     'howDoesItWork',
+    'whereDoesItWork',
     'whichKindOfRequests',
+    'canICreateRequestsForOthers',
+    'canICreateRequestsForMultipleAreas',
     'howToPay',
-    'howToBeSureMoney',
+    'howToBeSafe',
     'whoCanSeeRequests',
-    'howContact',
+    'howToContact',
+    'whatToKeepInMind',
     'whatToConsider',
     'doIGetPaid',
     'whatAboutSafety',
-    'whyShouldI',
-    'howDoIKnowQuarantine',
-    'howCanIHelp',
-  ];
-
-  const faqTranslationStringAboutUs = [
+    'howCanISupportQhero',
+    'howHelpPeopleWithoutInternet',
     'whoAreYou',
     'howIsDataUsed',
+    'howToDeleteMyRequest',
+    'iGetNoAnswers',
   ];
 
-  const buildFAQs = (arrayOfKeys) => arrayOfKeys.map((translationString) => (
-    <QA key={translationString} question={t(`views.faq.questions.${translationString}`)}>
-      {t(`views.faq.answers.${translationString}`)}
+  const QAwithLink = (props) => (
+    <QA key={props.tranlationKey} question={t(`views.faq.questions.${props.tranlationKey}`)}>
+      {t(`views.faq.answers.${props.tranlationKey}.preLink`)}
+      <Link to={t(`views.faq.answers.${props.tranlationKey}.url`)} className="text-secondary hover:underline">
+        {' '}
+        {t(`views.faq.answers.${props.tranlationKey}.link`)}
+        {' '}
+      </Link>
+      {t(`views.faq.answers.${props.tranlationKey}.postLink`)}
     </QA>
-  ));
+  )
+
+  const QA = (props) => (
+    <>
+      <h2 className="text-xl font-teaser mt-8">{props.question}</h2>
+      <p className="font-open-sans">{props.children}</p>
+    </>
+  )
+
+  function buildFAQ(translationString) {
+    if (i18n.exists(`views.faq.answers.${translationString}.preLink`)) {
+      return (
+        <QAwithLink key={translationString} tranlationKey={translationString} />
+      );
+    } else {
+      return (
+        <QA key={translationString} question={t(`views.faq.questions.${translationString}`)}>
+          {t(`views.faq.answers.${translationString}`)}
+        </QA>
+      );
+    }
+  }
+
+  function buildFAQs(arrayOfKeys) {
+    return arrayOfKeys.map((translationString) => buildFAQ(translationString));
+  }
 
   return (
     <div className="mb-10 p-4">
       <h1 className="text-2xl font-main mt-8">{t('views.faq.title')}</h1>
-
-      {buildFAQs(faqTranslationStringHowTo)}
-
-      {/* This FAQ needs special treatment because of the link in it. */}
-      <QA question={t('views.faq.howHelpPeopleWithoutInternet.question')}>
-        {t('views.faq.howHelpPeopleWithoutInternet.answer.preLink')}
-        <Link to="/" className="text-secondary hover:underline">
-          {' '}
-          {t('views.faq.howHelpPeopleWithoutInternet.answer.link')}
-          {' '}
-        </Link>
-        {t('views.faq.howHelpPeopleWithoutInternet.answer.postLink')}
-      </QA>
-
-      {buildFAQs(faqTranslationStringAboutUs)}
-
+      {buildFAQs(faqTranslationStrings)}
       <Footer />
     </div>
   );
