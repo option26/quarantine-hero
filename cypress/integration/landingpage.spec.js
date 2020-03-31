7e95af09635d401bf5e3f5da0157c4ef798d645f
jagaSto <gassner@stocard.de>
2020-03-31 16:34:29 +0200

    refactor loginVerifiedUser

--- cypress/integration/landingpage.spec.js
+++ cypress/integration/landingpage.spec.js
@@ -1,3 +1,5 @@
+import loginVerifiedUser from '../util/loginVerifiedUser';
+
 context('Landing Page', () => {
 
   describe('User is not logged in', () => {
@@ -24,24 +26,7 @@ context('Landing Page', () => {
   describe('User is logged in and his email address has been verified', () => {
 
     beforeEach(() => {
-      cy.visit('localhost:3000');
-
-      // TODO: This yells ANTI-PATTERN. We should find a way to fix this
-      cy.wait(2000);
-      cy.get('body').then(($body) => {
-        if ($body.find('[data-cy=btn-sign-out]').length > 0) {
-          cy.get('[data-cy=btn-sign-out]').click();
-        }
-      });
-
-      cy.visit('localhost:3000/#/signup');
-      cy.get('form input[type="email"]').type('florian.schmidt.1994@icloud.com{enter}');
-      cy.get('form input[type="password"]').type('test1234{enter}');
-
-      // TODO: Why do we redirect to ask-for-help here!
-      cy.hash().should('equal', '#/ask-for-help');
-
-      cy.visit('localhost:3000');
+      loginVerifiedUser('florian.schmidt.1994@icloud.com', 'test1234');
     });
 
     it('clicking on "Ich möchte Helfen" should redirect to overview', () => {
