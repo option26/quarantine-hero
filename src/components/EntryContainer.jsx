e7c3fcb1563de304bc124606eebc5152b9dca83f
Maurice22 <m.ackel@icloud.com>
2020-04-10 14:27:47 +0200

    Added functionality to specify a title of the component. This will push the buttons to the right and display the title on the left.

--- src/components/EntryContainer.jsx
+++ src/components/EntryContainer.jsx
@@ -3,36 +3,39 @@ import { useTranslation } from 'react-i18next';
 import EntryMap from './EntryMap';
 import EntryList from './EntryList';
 
-export default function EntryContainer({ pageSize }) {
+export default function EntryContainer({ pageSize, title }) {
   const { t } = useTranslation();
 
   const [isMapView, setIsMapView] = useState(false);
 
   return (
     <div>
-      <div className="flex flex-row justify-center">
-        <button
-          type="button"
-          onClick={() => {
-            setIsMapView(false);
-          }}
-          className={`text-white items-center rounded-l py-3 px-6 btn-main ${
-            isMapView ? 'btn-light-green' : 'btn-dark-green'
-          } hover:opacity-75`}
-        >
-          {t('components.filteredList.list')}
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            setIsMapView(true);
-          }}
-          className={`text-white items-center rounded-r py-3 px-6 btn-main ${
-            isMapView ? 'btn-dark-green' : 'btn-light-green'
-          } hover:opacity-75`}
-        >
-          {t('components.filteredList.map')}
-        </button>
+      <div className={`flex flex-row ${title ? 'justify-between' : 'justify-center'}`}>
+        {title && <div className="text-2xl font-bold font-open-sans">{title}</div>}
+        <div className="flex flex-row justify-center">
+          <button
+            type="button"
+            onClick={() => {
+              setIsMapView(false);
+            }}
+            className={`text-white items-center rounded-l py-3 px-6 btn-main ${
+              isMapView ? 'btn-light-green' : 'btn-dark-green'
+            } hover:opacity-75`}
+          >
+            {t('components.filteredList.list')}
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setIsMapView(true);
+            }}
+            className={`text-white items-center rounded-r py-3 px-6 btn-main ${
+              isMapView ? 'btn-dark-green' : 'btn-light-green'
+            } hover:opacity-75`}
+          >
+            {t('components.filteredList.map')}
+          </button>
+        </div>
       </div>
       {isMapView ? (
         <EntryMap />
