62c163f6130345699e5918f2676c9afe89bf2242
Maurice22 <m.ackel@icloud.com>
2020-04-11 12:08:48 +0200

    Added additional switch to be able to render components outside the main QH framework

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ import ShareButtons from './components/ShareButtons';
 import Press from './views/Press';
 import createEventListener from './util/createEventListener';
 import Security from './views/Security';
+import FilteredListFrame from './views/FilteredListFrame';
 
 function TopNavigation({ isAuthLoading, user, signOut }) {
   const { t } = useTranslation();
@@ -121,104 +122,111 @@ export default function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="flex items-center min-h-screen flex-col bg-kaki">
-      <Router>
-        <TopNavigation user={user} isAuthLoading={isAuthLoading} signOut={signOut} />
-        <div className="phone-width bg-white shadow-xl min-h-screen md:mt-6">
-          <ScrollToTop />
-          <DesktopMenu isLoggedIn={user} signOut={signOut} />
-          <div className="md:px-16 overflow-hidden">
-            <div style={{ zIndex: 101 }} className="visible md:invisible h-16 w-full fixed top-0 bg-white flex flex-row justify-between w-full items-center pr-5">
-              <Link to="/" className="font-main ml-4" style={{ fontWeight: '600' }}>
-                <img alt="logo" src={require('./assets/logo_invert.svg')} className="h-10" />
-              </Link>
-              <div>
-                <MenuIcon style={{ fontSize: '40px' }} className="text-gray-600" onClick={() => setMenuOpen(true)} />
+    <Router>
+      <Switch>
+        <Route exact path="/filtered-list-frame">
+          <FilteredListFrame />
+        </Route>
+        <Route path="*">
+          <div className="flex items-center min-h-screen flex-col bg-kaki">
+            <TopNavigation user={user} isAuthLoading={isAuthLoading} signOut={signOut} />
+            <div className="phone-width bg-white shadow-xl min-h-screen md:mt-6">
+              <ScrollToTop />
+              <DesktopMenu isLoggedIn={user} signOut={signOut} />
+              <div className="md:px-16 overflow-hidden">
+                <div style={{ zIndex: 101 }} className="visible md:invisible h-16 w-full fixed top-0 bg-white flex flex-row justify-between w-full items-center pr-5">
+                  <Link to="/" className="font-main ml-4" style={{ fontWeight: '600' }}>
+                    <img alt="logo" src={require('./assets/logo_invert.svg')} className="h-10" />
+                  </Link>
+                  <div>
+                    <MenuIcon style={{ fontSize: '40px' }} className="text-gray-600" onClick={() => setMenuOpen(true)} />
+                  </div>
+                </div>
+                <Sidebar open={menuOpen} onClose={() => setMenuOpen(false)} isLoggedIn={user} signOut={signOut} />
+                <div className="mt-20 md:mt-0">
+                  <Switch>
+                    <Route path="/offer-help/:id">
+                      <OfferHelp />
+                    </Route>
+                    <Route path="/signup/:returnUrl">
+                      <Signup />
+                    </Route>
+                    <Route path="/signup">
+                      <Signup />
+                    </Route>
+                    <Route path="/signin/:returnUrl">
+                      <Signin />
+                    </Route>
+                    <Route path="/signin">
+                      <Signin />
+                    </Route>
+                    <Route path="/verify-email">
+                      <VerifyEmail />
+                    </Route>
+                    <Route path="/ask-for-help">
+                      <AskForHelp />
+                    </Route>
+                    <Route path="/dashboard">
+                      <Dashboard />
+                    </Route>
+                    <Route path="/faq">
+                      <FAQ />
+                    </Route>
+                    <Route path="/security-tips">
+                      <Security />
+                    </Route>
+                    <Route path="/impressum">
+                      <Impressum />
+                    </Route>
+                    <Route path="/overview">
+                      <Overview />
+                    </Route>
+                    <Route path="/success">
+                      <Success />
+                    </Route>
+                    <Route path="/success-offer">
+                      <SuccessOffer />
+                    </Route>
+                    <Route path="/dsgvo">
+                      <DSGVO />
+                    </Route>
+                    <Route path={['/press', '/presse']}>
+                      <Press />
+                    </Route>
+                    <Route path="/notify-me">
+                      <NotifyMe />
+                    </Route>
+                    <Route path="/complete-offer-help">
+                      <CompleteOfferHelp />
+                    </Route>
+                    <Route exact path="/">
+                      <Main />
+                    </Route>
+                    <Route component={NotFound} path="*" />
+                  </Switch>
+                  <ScrollUpButton
+                    ContainerClassName="scroll-up-btn"
+                    TransitionClassName="scroll-up-btn-fade"
+                  >
+                    <img alt="arrow-down" className="arrow-down" src={require('./assets/arrows_up.svg')} />
+                  </ScrollUpButton>
+                </div>
               </div>
             </div>
-            <Sidebar open={menuOpen} onClose={() => setMenuOpen(false)} isLoggedIn={user} signOut={signOut} />
-            <div className="mt-20 md:mt-0">
-              <Switch>
-                <Route path="/offer-help/:id">
-                  <OfferHelp />
-                </Route>
-                <Route path="/signup/:returnUrl">
-                  <Signup />
-                </Route>
-                <Route path="/signup">
-                  <Signup />
-                </Route>
-                <Route path="/signin/:returnUrl">
-                  <Signin />
-                </Route>
-                <Route path="/signin">
-                  <Signin />
-                </Route>
-                <Route path="/verify-email">
-                  <VerifyEmail />
-                </Route>
-                <Route path="/ask-for-help">
-                  <AskForHelp />
-                </Route>
-                <Route path="/dashboard">
-                  <Dashboard />
-                </Route>
-                <Route path="/faq">
-                  <FAQ />
-                </Route>
-                <Route path="/security-tips">
-                  <Security />
-                </Route>
-                <Route path="/impressum">
-                  <Impressum />
-                </Route>
-                <Route path="/overview">
-                  <Overview />
-                </Route>
-                <Route path="/success">
-                  <Success />
-                </Route>
-                <Route path="/success-offer">
-                  <SuccessOffer />
-                </Route>
-                <Route path="/dsgvo">
-                  <DSGVO />
-                </Route>
-                <Route path={['/press', '/presse']}>
-                  <Press />
-                </Route>
-                <Route path="/notify-me">
-                  <NotifyMe />
-                </Route>
-                <Route path="/complete-offer-help">
-                  <CompleteOfferHelp />
-                </Route>
-                <Route exact path="/">
-                  <Main />
-                </Route>
-                <Route component={NotFound} path="*" />
-              </Switch>
-              <ScrollUpButton
-                ContainerClassName="scroll-up-btn"
-                TransitionClassName="scroll-up-btn-fade"
-              >
-                <img alt="arrow-down" className="arrow-down" src={require('./assets/arrows_up.svg')} />
-              </ScrollUpButton>
-            </div>
+            <CookieConsent
+              location="bottom"
+              buttonText="Okay"
+              cookieName="cookieConsent"
+              style={{ background: '#2B373B' }}
+              buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+              onAccept={enableAnalytics}
+              expires={365}
+            >
+              {t('App.usesCookies')}
+            </CookieConsent>
           </div>
-        </div>
-      </Router>
-      <CookieConsent
-        location="bottom"
-        buttonText="Okay"
-        cookieName="cookieConsent"
-        style={{ background: '#2B373B' }}
-        buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
-        onAccept={enableAnalytics}
-        expires={365}
-      >
-        {t('App.usesCookies')}
-      </CookieConsent>
-    </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
