fe7b9a786a1c03e6cf2134a97669ee5b5e05b5d7
Tom Graupner <super-git@tomgraupner.de>
2020-04-14 00:24:05 +0200

    Added cypress tests to pull request action

--- .github/workflows/build-frontend.sh
+++ .github/workflows/build-frontend.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-rm -rf build && \
-rm -rf node_modules && \
-yarn install && \
-yarn lint && \
+rm -rf build
+rm -rf node_modules
+yarn install
+yarn lint
+yarn test-mobile
+yarn test-desktop
 NODE_ENV=prod yarn build
