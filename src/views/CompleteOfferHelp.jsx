18143d5353f34d3780e5ebd099936f239dff4cbf
Maurice22 <m.ackel@icloud.com>
2020-04-16 16:36:25 +0200

    Added error handling for invalid signup link

--- src/views/CompleteOfferHelp.jsx
+++ src/views/CompleteOfferHelp.jsx
@@ -7,10 +7,45 @@ import { useTranslation } from 'react-i18next';
 import fb from '../firebase';
 import { isMapsApiEnabled } from '../featureFlags';
 import { getGeodataForPlace, getGeodataForString, getLatLng } from '../services/GeoService';
+import Loader from '../components/loader/Loader';
 
 export default function CompleteOfferHelp() {
   const { t } = useTranslation();
+  const [isLoading, setLoading] = useState(true);
   const [location, setLocation] = useState('');
+  const [error, setError] = useState(undefined);
+
+  const createOfferHelp = async (loc, placeId, email) => {
+    const geofirestore = new GeoFirestore(fb.store);
+    const offerHelpCollection = geofirestore.collection('/offer-help');
+    let lat = 0;
+    let lng = 0;
+    let plz = loc;
+    if (isMapsApiEnabled) {
+      let results;
+      if (placeId) {
+        results = await getGeodataForPlace(placeId);
+      } else {
+        results = await getGeodataForString(loc);
+      }
+      const plzComponent = results[0].address_components.find((c) => c.types.includes('postal_code'));
+      if (plzComponent) plz = plzComponent.short_name;
+      const coordinates = getLatLng(results[0]);
+      lat = coordinates.lat;
+      lng = coordinates.lng;
+    }
+
+    await offerHelpCollection.add({
+      email,
+      location: loc,
+      plz,
+      uid: firebase.auth().currentUser.uid,
+      timestamp: Date.now(),
+      coordinates: new fb.app.firestore.GeoPoint(lat, lng),
+    });
+
+    fb.analytics.logEvent('success_subscribe_region_confirmation');
+  };
 
   useEffect(() => {
     async function completeSignup() {
@@ -29,44 +64,20 @@ export default function CompleteOfferHelp() {
           email = window.prompt(t('views.completeOfferHelp.mailYouRegistered'));
         }
 
-        await fb.auth.signInWithEmailLink(email, window.location.href);
-        window.localStorage.removeItem('emailForSignIn');
+        try {
+          await fb.auth.signInWithEmailLink(email, window.location.href);
+          window.localStorage.removeItem('emailForSignIn');
 
-        const urlParams = new URLSearchParams(window.location.hash);
+          const urlParams = new URLSearchParams(window.location.hash);
+          const loc = urlParams.get('#/complete-offer-help?location');
+          const placeId = urlParams.get('#/complete-offer-help?placeId');
 
-        const loc = urlParams.get('#/complete-offer-help?location');
-        const placeId = urlParams.get('#/complete-offer-help?placeId');
-        setLocation(loc);
-
-        const geofirestore = new GeoFirestore(fb.store);
-        const offerHelpCollection = geofirestore.collection('/offer-help');
-        let lat = 0;
-        let lng = 0;
-        let plz = loc;
-        if (isMapsApiEnabled) {
-          let results;
-          if (placeId) {
-            results = await getGeodataForPlace(placeId);
-          } else {
-            results = await getGeodataForString(loc);
-          }
-          const plzComponent = results[0].address_components.find((c) => c.types.includes('postal_code'));
-          if (plzComponent) plz = plzComponent.short_name;
-          const coordinates = getLatLng(results[0]);
-          lat = coordinates.lat;
-          lng = coordinates.lng;
+          setLocation(loc);
+          createOfferHelp(loc, placeId, email);
+        } catch (err) {
+          setError(err);
         }
-
-        await offerHelpCollection.add({
-          email,
-          location: loc,
-          plz,
-          uid: firebase.auth().currentUser.uid,
-          timestamp: Date.now(),
-          coordinates: new fb.app.firestore.GeoPoint(lat, lng),
-        });
-
-        fb.analytics.logEvent('success_subscribe_region_confirmation');
+        setLoading(false);
       }
     }
 
@@ -75,18 +86,35 @@ export default function CompleteOfferHelp() {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-exo2 mt-10 mb-6">{t('views.completeOfferHelp.youAreHero')}</h1>
-      <p>
-        {t('views.completeOfferHelp.mailVerified')}
-        {' '}
-        <span className="text-secondary">{location}</span>
-        {' '}
-        {t('views.completeOfferHelp.needsHelp')}
-      </p>
-      <div className="flex justify-center flex-col items-center mb-8">
-        <img className="h-48 w-48 my-10" src={require('../assets/success.svg')} alt="" />
-        <Link className="btn-green mt-10" to="/dashboard">{t('views.completeOfferHelp.toYourOverview')}</Link>
-      </div>
+      {isLoading && <Loader />}
+      {!isLoading && error && (
+        <>
+          <h1 className="text-2xl font-exo2 mt-10 mb-6">{t('views.completeOfferHelp.anErrorOccured')}</h1>
+          <p>
+            {t('views.completeOfferHelp.verificationError')}
+          </p>
+          <div className="flex justify-center flex-col items-center mb-8">
+            <img className="h-48 w-48 my-10" src={require('../assets/error.svg')} alt="" />
+            <Link className="btn-green mt-10" to="/notify-me">{t('views.completeOfferHelp.tryAgain')}</Link>
+          </div>
+        </>
+      )}
+      {!isLoading && !error && (
+        <>
+          <h1 className="text-2xl font-exo2 mt-10 mb-6">{t('views.completeOfferHelp.youAreHero')}</h1>
+          <p>
+            {t('views.completeOfferHelp.mailVerified')}
+            {' '}
+            <span className="text-secondary">{location}</span>
+            {' '}
+            {t('views.completeOfferHelp.needsHelp')}
+          </p>
+          <div className="flex justify-center flex-col items-center mb-8">
+            <img className="h-48 w-48 my-10" src={require('../assets/success.svg')} alt="" />
+            <Link className="btn-green mt-10" to="/dashboard">{t('views.completeOfferHelp.toYourOverview')}</Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
