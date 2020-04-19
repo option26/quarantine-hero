5d67f7fac45ce66f5b689d876f58c0f6e9a50780
Maurice22 <m.ackel@icloud.com>
2020-04-19 22:23:50 +0200

    Handle expired or invalid email links correctly (#234)
    * Added error text for complete offer help and fixed typo
    * Moved loader to subfolder and added actual loading animation
    * Fixed loader import
    * Added error handling for invalid signup link
    * Added error svg
    * Added early return and fixed error with infinit loader on the way
    
    Close #191

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Loader = () => (
-  <div className="App">
-    <div className="flex items-center min-h-screen flex-col bg-kaki" />
-  </div>
-);
-
-export default Loader;
