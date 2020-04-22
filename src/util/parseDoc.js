bef871cda85252cf23be534a61efec9f962abc1a
Maurice22 <m.ackel@icloud.com>
2020-04-22 20:53:53 +0200

    Fixed data retrieval for geofirestore (#243)

--- src/util/parseDoc.js
+++ src/util/parseDoc.js
@@ -2,7 +2,8 @@ import * as Sentry from '@sentry/browser';
 
 export default function parseDoc(doc) {
   try {
-    return { ...doc.data().d, id: doc.id };
+    const data = doc.data();
+    return { ...(data.d || data), id: doc.id };
   } catch (err) {
     Sentry.captureException(new Error(`Error parsing ask-for-help ${doc.id}`));
     return null;
