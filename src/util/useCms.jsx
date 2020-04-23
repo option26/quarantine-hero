38cc126743bb23df20ca0be4fccf910848084dc2
Maurice22 <m.ackel@icloud.com>
2020-04-23 11:39:04 +0200

    Moved functions inside hook

--- src/util/useCms.jsx
+++ src/util/useCms.jsx
@@ -6,33 +6,33 @@ export default function useCms(collectionName) {
   const [content, setContent] = useState([]);
   const { languages } = i18next;
 
-  const getContent = async (collName, lang) => (
-    new Promise((resolve, reject) => {
-      const contentRef = firebase.database().ref(`${collName}/${lang}`);
-
-      contentRef.once('value', (snapshot) => {
-        if (snapshot.val() === null) {
-          return reject(new Error('Undefined key'));
-        }
-        return resolve(contentRef);
-      });
-    })
-  );
-
-  const getInternationalizedContent = async () => {
-    for (let i = 0; i < languages.length; i += 1) {
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        const contentRef = await getContent(collectionName, languages[i]);
-        contentRef.on('value', (snapshot) => setContent(snapshot.val()));
-        return;
-      } catch (err) {} // eslint-disable-line no-empty
-    }
-  };
-
   useEffect(() => {
+    const getContent = async (collName, lang) => (
+      new Promise((resolve, reject) => {
+        const contentRef = firebase.database().ref(`${collName}/${lang}`);
+
+        contentRef.once('value', (snapshot) => {
+          if (snapshot.val() === null) {
+            return reject(new Error('Undefined key'));
+          }
+          return resolve(contentRef);
+        });
+      })
+    );
+
+    const getInternationalizedContent = async () => {
+      for (let i = 0; i < languages.length; i += 1) {
+        try {
+          // eslint-disable-next-line no-await-in-loop
+          const contentRef = await getContent(collectionName, languages[i]);
+          contentRef.on('value', (snapshot) => setContent(snapshot.val()));
+          return;
+        } catch (err) { } // eslint-disable-line no-empty
+      }
+    };
+
     getInternationalizedContent();
-  }, []);
+  }, [collectionName, languages]);
 
   return [content];
 }
