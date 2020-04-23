286bd699d8051d3549811678efba3cb3227ce442
Maurice22 <m.ackel@icloud.com>
2020-04-23 22:24:57 +0200

    Fixed ask for help creation and added default msg

--- cypress/util/createAskForHelpPosting.js
+++ cypress/util/createAskForHelpPosting.js
@@ -1,6 +1,6 @@
-export default function createAskForHelpPosting(postalCode, description) {
+export default function createAskForHelpPosting(postalCode, description = `Test ${new Date().toISOString()}`) {
   cy.visit('localhost:3000/#/ask-for-help');
-  cy.get('[data-cy=location-search-input]').type(`${postalCode}{enter}`);
+  cy.get('[data-cy=location-search-input]').type(`${postalCode}`);
   cy.get('[data-cy=autocomplete-suggestion]').first().click();
   cy.get('[data-cy=ask-for-help-text-input]').type(description);
   cy.get('[data-cy=ask-for-help-submit]').click();
--- src/components/LocationInput.jsx
+++ src/components/LocationInput.jsx
@@ -258,6 +258,7 @@ function AutocompleteSuggestion(props) {
     <button
       ref={ref}
       type="button"
+      data-cy="autocomplete-suggestion"
       className="px-3 py-2 hover:bg-gray-100 focus:bg-gray-100 cursor-pointer w-full text-left"
       onClick={onClick}
     >
