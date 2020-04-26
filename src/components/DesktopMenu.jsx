f94295eb4c63e47b7ab3ddffbc0b43b95ee8dd86
Maurice22 <m.ackel@icloud.com>
2020-04-26 22:25:48 +0200

    Removed unnecessary dependency, moved FAQs to top navigation

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import { useTranslation } from 'react-i18next';
 
 export default function DesktopMenu() {
@@ -13,24 +12,21 @@ export default function DesktopMenu() {
   );
 
   const Menu = () => (
-    <Box className="flex w-1/2" flexDirection="column">
-      <ul className="font-exo2 flex justify-around text-base font-semibold mt-4 mr-4 -ml-16">
+    <div className="flex justify-end">
+      <ul className="font-exo2 flex justify-around text-xl font-semibold mt-4 mr-2">
         <MenuItem to="/ask-for-help">{t('components.desktopMenu.requestHelp')}</MenuItem>
         <MenuItem to="/overview">{t('components.desktopMenu.help')}</MenuItem>
         <MenuItem to="/security-tips">{t('components.desktopMenu.safety')}</MenuItem>
-        <MenuItem to="/faq">FAQs</MenuItem>
       </ul>
-    </Box>
+    </div>
   );
 
   return (
-    <>
-      <div className="hidden md:flex justify-between relative">
-        <Link className="-ml-8 block -mt-12" to="/">
-          <img alt="logo" src={require('../assets/logo_mark.svg')} />
-        </Link>
-        <Menu />
-      </div>
-    </>
+    <div className="hidden md:flex justify-between relative">
+      <Link className="-ml-16 block -mt-12" to="/">
+        <img alt="logo" src={require('../assets/logo_mark.svg')} />
+      </Link>
+      <Menu />
+    </div>
   );
 }
