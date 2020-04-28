a0489f2795103a730071ed706c4fe9df6f2673a2
Maurice22 <m.ackel@icloud.com>
2020-04-28 21:18:43 +0200

    taking search parameter string directly from router location now

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -121,7 +121,7 @@ export default function EntryList({ pageSize = 0 }) {
 
   useEffect(() => {
     // Because we use the hash router, we cannot use the default functionality here as it would expect the query parameters before the hash
-    const urlParams = new URLSearchParams(window.location.hash.split('?')[1]);
+    const urlParams = new URLSearchParams(windowLocation.search);
     const address = urlParams.get('address');
 
     if (address) {
