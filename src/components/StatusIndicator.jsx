35cd0849358f5225df53f34e755eda47c4c82dab
Maurice22 <m.ackel@icloud.com>
2020-04-29 00:35:25 +0200

    Added default layout that can be overwritten by passing children

--- src/components/StatusIndicator.jsx
+++ src/components/StatusIndicator.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export default function StatusIndicator(props) {
   const {
-    text,
-    continueURL = '/',
-    continueText = 'Zur StartSeite',
     success = true,
+    text,
+    children,
   } = props;
+
+  const { t } = useTranslation();
 
   const imgSource = success ? require('../assets/success.svg') : require('../assets/error.svg');
 
@@ -17,7 +19,7 @@ export default function StatusIndicator(props) {
         <p className="text-2xl font-teaser mb-8 text-center">{text}</p>
         <div className="flex justify-center flex-col items-center mb-8">
           <img className="h-48 w-48 my-10" src={imgSource} alt="" />
-          <Link className="btn-green mt-10" to={continueURL}>{continueText}</Link>
+          {children || <Link className="btn-green mt-10" to="/">{t('components.statusIndicator.toHome')}</Link>}
         </div>
       </div>
     </div>
