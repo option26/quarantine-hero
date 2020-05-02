0c655c8c1d257a2ffaa98432766f69aeb18be41f
Maurice22 <m.ackel@icloud.com>
2020-05-02 15:23:41 +0200

    Added function to whitelabel the signup page for partners

--- src/views/Signup.jsx
+++ src/views/Signup.jsx
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { useDocumentDataOnce } from 'react-firebase-hooks/firestore';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import {
@@ -8,32 +9,109 @@ import {
   useLocation,
 } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import MailInput from '../components/MailInput';
 import { baseUrl } from '../appConfig';
 
-export default () => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [error, setError] = React.useState('');
-  const [user] = useAuthState(firebase.auth());
-  const location = useLocation();
-  const passwordInput = useRef();
-  const passwordRepeatInput = useRef();
+export default () => (
+  <div className="p-4">
+    <SignupHeader />
+    <SignupBody />
+  </div>
+);
+
+// We use an array to be able to manage this via the CMS in the future
+const partners = [
+  { key: 'nachbarhilft', name: 'Nachbar Hilft!', imgSource: require('../assets/150.png') },
+  { key: 'test', name: 'Im a partner', imgSource: require('../assets/br.jpg') },
+];
+
+function SignupHeader() {
   const { t } = useTranslation();
   const { returnUrl } = useParams();
+  const location = useLocation();
 
-  const createUserWithEmailAndPassword = (mail, pw) => firebase.auth().createUserWithEmailAndPassword(mail, pw);
+  const [source, setSource] = useState(undefined);
+  const [user] = useAuthState(firebase.auth());
+
+  const [externalStats] = useDocumentDataOnce(firebase.firestore().collection('stats').doc('external'));
+
+  const reasonForSignup = location && location.state && location.state.reason_for_registration
+    ? location.state.reason_for_registration
+    : t('views.signUp.reasonForSignupDefault');
+  const headerText = t('views.signUp.headerText', { reasonForSignup });
+
+  useEffect(() => {
+    const query = new URLSearchParams(location.search);
+    if (query.has('source')) {
+      setSource(query.get('source'));
+    }
+  }, [location]);
+
 
   if (user) {
     if (returnUrl) return <Redirect to={`/${decodeURIComponent(returnUrl)}`} />;
     return user.emailVerified ? <Redirect to="/ask-for-help" /> : <Redirect to="/verify-email" />;
   }
 
-  const reasonForSignup = location && location.state && location.state.reason_for_registration
-    ? location.state.reason_for_registration
-    : t('views.signUp.reasonForSignupDefault');
-  const headerText = t('views.signUp.headerText', { reasonForSignup });
+  const partner = partners.find((p) => p.key === source);
+  if (source && partner) {
+    const { name: partnerName, imgSource } = partner;
+    if (partnerName && imgSource) {
+      const partnerText = (
+        <Trans i18nKey="views.signUp.partnerText">
+          {{ helpers: externalStats && externalStats.regionSubscribed }}
+          {{ partnerName }}
+        </Trans>
+      );
+
+      return (
+        <div className="mt-8 mb-6">
+          <div className="flex flex-row md:flex-col-reverse">
+            <div className="flex flex-shrink-0 items-center">
+              <img className="rounded-full w-24 h-24" src={imgSource} alt="" />
+              <p className="ml-4 hidden md:block align-middle">
+                {partnerText}
+              </p>
+            </div>
+            <div className="m-2" />
+            <div style={{ hyphens: 'manual' }} className="font-teaser">
+              {t('views.signUp.headerTextPartner')}
+            </div>
+          </div>
+          <p className="mt-4 md:hidden">
+            {partnerText}
+          </p>
+          <p className="mt-4">
+            {headerText}
+            {' '}
+            {t('views.signUp.headerTextExtended')}
+          </p>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="mt-8 mb-6">
+      <div className="font-teaser">
+        {headerText}
+      </div>
+    </div>
+  );
+}
+
+function SignupBody() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState('');
+  const passwordInput = useRef();
+  const passwordRepeatInput = useRef();
+  const { t } = useTranslation();
+  const { returnUrl } = useParams();
+  const location = useLocation();
+
+  const createUserWithEmailAndPassword = (mail, pw) => firebase.auth().createUserWithEmailAndPassword(mail, pw);
 
   const registerUser = async (e) => {
     // Prevent page reload
@@ -61,12 +139,9 @@ export default () => {
   };
 
   return (
-    <div className="p-4 mt-8">
+    <>
       <form onSubmit={registerUser}>
         <div className="mb-4">
-          <div className="font-teaser mb-6">
-            {headerText}
-          </div>
           <label className="block text-gray-700 text-sm font-bold mb-1 font-open-sans" htmlFor="username">
             {t('views.signUp.email')}
           </label>
@@ -131,6 +206,6 @@ export default () => {
       >
         {t('views.signUp.haveAnAccount')}
       </Link>
-    </div>
+    </>
   );
-};
+}
