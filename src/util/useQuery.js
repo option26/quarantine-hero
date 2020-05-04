177090bb7d05a2ac41ad5b57b2b92468269bf52b
Maurice22 <m.ackel@icloud.com>
2020-05-04 21:41:57 +0200

    Put calculation inside an effect hook to prevent recomputation

--- src/util/useQuery.js
+++ src/util/useQuery.js
@@ -1,11 +1,19 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default () => {
-  const paramObj = {};
-  const paramArray = [...new URLSearchParams(useLocation().search).entries(), ...new URLSearchParams(window.location.search)];
-  paramArray.forEach((entry) => {
-    const [key, value] = entry;
-    paramObj[key] = value;
-  });
-  return paramObj;
+  const [paramObject, setParamObject] = useState({});
+  const location = useLocation();
+
+  useEffect(() => {
+    const paramArray = [...new URLSearchParams(location.search).entries(), ...new URLSearchParams(window.location.search)];
+    const newParamObject = paramArray.reduce((obj, entry) => {
+      const [key, value] = entry;
+      obj[key] = value; // eslint-disable-line no-param-reassign
+      return obj;
+    }, {});
+    setParamObject(newParamObject);
+  }, [location]);
+
+  return paramObject;
 };
