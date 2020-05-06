7569584518afc786310c5ffa9dec4d540b97db7d
Tom Graupner <tgraupne@users.noreply.github.com>
2020-05-06 23:55:19 +0200

    Made mobile the first test case for cypress and updated the github ac… (#227)
    * Made mobile the first test case for cypress and updated the github action for prs
    
    * Fixed cypress tests
    
    * updated test cases with different workflows for mobile and desktop
    * added new data-cy properties to menu icons and sidebar entries
    
    * Incorporated PR feedback
    
    * Skipping flaky test and moved backend-related yarn scripts to package.json in backend subfolder
    
    * Fixed typo
    
    * Fixed broken eslint command
    
    * Incorporated PR feedback
    
    * Incorporated PR feedback
    
    * changed wording from "backend" to "firebase"
    * deactivated video capturing
    
    Co-authored-by: Tom Graupner <super-git@tomgraupner.de>
    Co-authored-by: Maurice22 <m.ackel@icloud.com>

--- .github/workflows/deploy-firebase.sh
+++ .github/workflows/deploy-firebase.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+# exit script as soon as any command returns non-zero
+set -e
 
 if [[ -z "$FIREBASE_TOKEN" ]]; then
     echo "Set the FIREBASE_TOKEN env variable."
     exit 1
 fi
 
-GIT_REV="$(git rev-parse HEAD)"&& \
-FIREBASE_PROJECT="quarantine-hero" && \
-FIREBASE_MESSAGE="QH $GIT_REV deployment to firebase" && \
-cd ./firebase/functions && \
+GIT_REV="$(git rev-parse HEAD)"
+FIREBASE_PROJECT="quarantine-hero"
+FIREBASE_MESSAGE="QH $GIT_REV deployment to firebase"
 # deploy here because npm does not resolve environment variables in npm run
 npx firebase-tools deploy --only functions --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
--- cypress/integration/landingpage.spec.js
+++ cypress/integration/landingpage.spec.js
@@ -1,3 +1,6 @@
+const verifiedEmailAdress = 'verified@example.com';
+const password = 'test1234';
+
 context('Landing Page', () => {
 
   describe('User is not logged in', () => {
@@ -30,8 +33,8 @@ context('Landing Page', () => {
       indexedDB.deleteDatabase('firebaseLocalStorageDb');
 
       cy.visit('localhost:3000/#/signin');
-      cy.get('form input[type="email"]').type('florian.schmidt.1994@icloud.com{enter}');
-      cy.get('form input[type="password"]').type('test1234{enter}');
+      cy.get('form input[type="email"]').type(`${verifiedEmailAdress}{enter}`);
+      cy.get('form input[type="password"]').type(`${password}{enter}`);
 
       // TODO: Why do we redirect to ask-for-help here!
       cy.hash().should('equal', '#/ask-for-help');
@@ -49,23 +52,42 @@ context('Landing Page', () => {
       cy.hash().should('equal', '#/ask-for-help');
     });
 
-    it('clicking on an entry should redirect to offer help', () => {
+    // TODO: The redirect depends on the ownership of the entry.
+    //  We need to split this and find a way to prepare the test
+    it.skip('clicking on an entry should redirect to dashboard', () => {
       cy.get('.entry').first().click();
-      cy.hash().should('contain', '#/offer-help/');
+      cy.hash().should('contain', '#/dashboard');
     });
 
     it('clicking on "Meine Übersicht" should redirect to dashboard', () => {
-      cy.get('[data-cy=nav-my-overview]').click();
+      if (Cypress.env('VIEWPORT') === 'desktop') {
+        cy.get('[data-cy=nav-my-overview]').click();
+      } else if (Cypress.env('VIEWPORT') === 'mobile') {
+        cy.get('[data-cy=mobile-menu-icon]').click();
+        cy.get('[data-cy=mobile-nav-my-overview]').click();
+      } else {
+        throw new Error('Unknown environment variable VIEWPORT');
+      }
       cy.hash().should('equal', '#/dashboard');
     });
 
     it('clicking on "Logout" should log out the user', () => {
       cy.server();
       cy.route('POST', 'https://www.googleapis.com/**').as('signOutUser');
-
-      cy.get('[data-cy=btn-sign-out]').click();
-      cy.get('[data-cy=btn-sign-out]').should('not.exist');
-      cy.get('[data-cy=nav-my-overview]').should('not.exist');
+      if (Cypress.env('VIEWPORT') === 'desktop') {
+        cy.get('[data-cy=btn-sign-out]').click();
+        cy.get('[data-cy=btn-sign-out]').should('not.exist');
+        cy.get('[data-cy=nav-my-overview]').should('not.exist');
+      } else if (Cypress.env('VIEWPORT') === 'mobile') {
+        cy.get('[data-cy=mobile-menu-icon]').click();
+        cy.get('[data-cy=mobile-nav-sign-out]').should('be.visible');
+        cy.get('[data-cy=mobile-nav-sign-out]').click();
+        cy.get('[data-cy=mobile-menu-icon]').click();
+        cy.get('[data-cy=mobile-nav-sign-out]').should('not.exist');
+        cy.get('[data-cy=mobile-nav-my-overview]').should('not.exist');
+      } else {
+        throw new Error('Unknown environment variable VIEWPORT');
+      }
     });
   });
 });
