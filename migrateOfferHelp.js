86059bfa38226bfef1b54ae0de1a90ae0cd758e1
Maurice <m.ackel@icloud.com>
2020-05-22 23:07:19 +0200

    Taken max batch sizes into account

--- migrateOfferHelp.js
+++ migrateOfferHelp.js
@@ -1,7 +1,7 @@
 const admin = require('./firebase/functions/node_modules/firebase-admin');
 
 const doMigration = async () => {
-  const serviceAccount = require('/Users/dev/Downloads/qhero-stage-firebase-adminsdk-e3ogb-355e92f39d.json');
+  const serviceAccount = require('<TODO>');
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -9,14 +9,19 @@ const doMigration = async () => {
   });
   const data = await admin.firestore().collection('offer-help').get();
 
-  const batch = admin.firestore().batch();
+  let batch = admin.firestore().batch();
 
-  data.docs.forEach((doc) => {
+  for (let i = 0; i < data.docs.length; i += 1) {
+    const doc = data.docs[i];
     const ref = admin.firestore().collection('notifications').doc(doc.id);
     batch.set(ref, doc.data());
-  });
 
-  await batch.commit();
+    // Be well under the limit of 500 operations in one batch
+    if ((i + 1) % 400 === 0) {
+      batch.commit();
+      batch = admin.firestore().batch();
+    }
+  }
 };
 
 doMigration();
