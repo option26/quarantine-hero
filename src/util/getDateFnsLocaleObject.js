75b85e82390a81c3e1c5abefc7736d237714d625
jakob-ed <jakob.ed@pm.me>
2020-06-06 14:19:44 +0200

    Limit imports to supported locales, fallback from regional locale to base locale

--- src/util/getDateFnsLocaleObject.js
+++ src/util/getDateFnsLocaleObject.js
@@ -1,12 +1,16 @@
-import * as locales from 'date-fns/locale';
+import { de, enUS } from 'date-fns/locale';
 import i18next from 'i18next';
+
+const locales = { 'de': de, 'en': enUS };
+
+const cutOffFromChar = (string, char) => string.indexOf(char) === -1 ? string : string.substring(0, string.indexOf(char));
 
 const getDateFnsLocaleObject = () => {
   const { languages } = i18next;
   const selectedLanguage = languages[0];
   const fallbackLanguage = languages[languages.length-1];
 
-  return locales[selectedLanguage.replace('-', '')] || locales[fallbackLanguage.replace('-', '')];
+  return locales[cutOffFromChar(selectedLanguage, '-')] || locales[cutOffFromChar(fallbackLanguage, '-')];
 }
 
 export default getDateFnsLocaleObject;
