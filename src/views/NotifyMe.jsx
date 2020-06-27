a2da3342a7e2d5063c3dc19fba15f368e244bce5
Maurice <m.ackel@icloud.com>
2020-06-27 13:39:15 +0200

    Updated style according to mockup

--- src/views/NotifyMe.jsx
+++ src/views/NotifyMe.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
 import * as Sentry from '@sentry/browser';
 import fb from '../firebase';
 import LocationInput from '../components/LocationInput';
 import MailInput from '../components/MailInput';
 import { baseUrl } from '../appConfig';
+import HelpNeededLogo from '../assets/help_needed.svg';
 
 export default function NotifyMe() {
   const { t } = useTranslation();
@@ -48,13 +50,20 @@ export default function NotifyMe() {
     return (
       <div className="p-4">
         <div className="font-teaser my-6">
-          {t('views.notifyMe.beNotified')}
+          {t('views.notifyMe.finalizeNotification')}
         </div>
-        <div className="border bg-secondary px-4 py-2 rounded text-white flex flex-row items-center border">
-          {t('views.notifyMe.emailSent', { location })}
+        <div className="px-4 py-2 font-semibold rounded text-secondary bg-light-green flex items-center">
+          {t('views.notifyMe.emailSent')}
+        </div>
+        <div className="mt-6 flex flex-col items-center">
+          <p className="font-light text-center">{t('views.notifyMe.willNotify')}</p>
+          <img className="mt-4" src={HelpNeededLogo} alt="" />
+          <p className="text-xl text-primary font-exo2 font-semibold">{location}</p>
+        </div>
+        <div className="flex mt-10 justify-center">
+          <Link className="btn-green" to="/overview">{t('views.notifyMe.findRequests')}</Link>
         </div>
       </div>
-
     );
   }
 
