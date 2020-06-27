f47378c702de806b5855c9ba9323a386bb35c9f9
jagaSto <gassner@stocard.de>
2020-06-27 16:24:35 +0200

    add config

---
+++ firebase/functions/src/config.ts
@@ -0,0 +1,27 @@
+import * as functions from 'firebase-functions';
+import * as sgMail from '@sendgrid/mail';
+
+const envVariables = functions.config();
+
+const sgMailApiKey = envVariables && envVariables.sendgrid && envVariables.sendgrid.key
+  ? envVariables.sendgrid.key
+  : null;
+sgMail.setApiKey(sgMailApiKey);
+
+const REGION_EUROPE_WEST_1 = 'europe-west1';
+const MAX_RESULTS = 30;
+const MAPS_ENABLED = true;
+const MINIMUM_NOTIFICATION_DELAY = 20;
+const SEND_EMAILS = sgMailApiKey !== null;
+const sendingMailsDisabledLogMessage = 'Sending emails is currently disabled.';
+const EMAIL_NOTIFICATION_AUDIENCE_SIZE_SANITY_CHECK = 35_000;
+
+export {
+  REGION_EUROPE_WEST_1,
+  MAX_RESULTS,
+  MAPS_ENABLED,
+  MINIMUM_NOTIFICATION_DELAY,
+  SEND_EMAILS,
+  sendingMailsDisabledLogMessage,
+  EMAIL_NOTIFICATION_AUDIENCE_SIZE_SANITY_CHECK,
+}
