8a099180cdd6b7198e1ed9a5a9ab56832950439d
Maurice <m.ackel@icloud.com>
2020-06-27 14:47:27 +0200

    Made FAQs collapsable and refactored component (#337)

--- src/views/FAQ.jsx
+++ src/views/FAQ.jsx
@@ -1,34 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function FAQ() {
   const { t, i18n } = useTranslation();
-
-  const QAwithLink = ({ translationKey }) => {
-    const hasPostLink = i18n.exists(`views.faq.answers.${translationKey}.postLink`);
-    const postLinkText = t(`views.faq.answers.${translationKey}.postLink`);
-    const postLinkIsNotEmptyOrFullStop = postLinkText !== '' && postLinkText !== '.';
-
-    return (
-      <QA key={translationKey} question={t(`views.faq.questions.${translationKey}`)}>
-        {t(`views.faq.answers.${translationKey}.preLink`)}
-        <Link to={t(`views.faq.answers.${translationKey}.url`)} className="text-secondary hover:underline">
-          {' '}
-          {t(`views.faq.answers.${translationKey}.link`)}
-          {hasPostLink && postLinkIsNotEmptyOrFullStop ? ' ' : ''}
-        </Link>
-        {postLinkText}
-      </QA>
-    );
-  };
-
-  const QA = ({ question, children }) => (
-    <>
-      <h2 className="text-xl font-teaser mt-8">{question}</h2>
-      <p className="font-open-sans text-justify">{children}</p>
-    </>
-  );
 
   function buildFAQ(translationString) {
     return (i18n.exists(`views.faq.answers.${translationString}.preLink`))
@@ -45,5 +23,55 @@ export default function FAQ() {
       <h1 className="text-2xl font-main mt-8">{t('views.faq.title')}</h1>
       {buildFAQs(Object.keys(t('views.faq.questions', { returnObjects: true })))}
     </div>
+  );
+}
+
+function QA({ question, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        type="button"
+        className="mt-4 mb-1 px-4 py-2 flex justify-start items-center bg-kaki w-full focus:outline-none"
+        onClick={() => {
+          setIsOpen((current) => !current);
+        }}
+      >
+        <div className="font-semibold text-left mr-4">{question}</div>
+        <div className="flex-1" />
+        {
+          React.createElement((isOpen ? ExpandLessIcon : ExpandMoreIcon), {
+            className: 'cursor-pointer hover:opacity-50',
+            style: { fontSize: '40px' },
+          })
+        }
+      </button>
+      <Collapse in={isOpen}>
+        <div className="p-4 bg-kaki">
+          {children}
+        </div>
+      </Collapse>
+    </>
+  );
+}
+
+function QAwithLink({ translationKey }) {
+  const { t, i18n } = useTranslation();
+
+  const hasPostLink = i18n.exists(`views.faq.answers.${translationKey}.postLink`);
+  const postLinkText = t(`views.faq.answers.${translationKey}.postLink`);
+  const postLinkIsNotEmptyOrFullStop = postLinkText !== '' && postLinkText !== '.';
+
+  return (
+    <QA key={translationKey} question={t(`views.faq.questions.${translationKey}`)}>
+      {t(`views.faq.answers.${translationKey}.preLink`)}
+      <Link to={t(`views.faq.answers.${translationKey}.url`)} className="text-secondary hover:underline">
+        {' '}
+        {t(`views.faq.answers.${translationKey}.link`)}
+        {hasPostLink && postLinkIsNotEmptyOrFullStop ? ' ' : ''}
+      </Link>
+      {postLinkText}
+    </QA>
   );
 }
