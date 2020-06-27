22bc92f8a0b0ae3ce2bf40529eb6b09f3716175a
Keno Dressel <hire@keno.digital>
2020-06-27 15:25:20 +0200

    adds options to accept or reject cookies (#336)
    * adds options to accept or reject cookies
    
    * now links to privacy policy
    
    * cookie consent no longer covering impressum on mobile

--- src/util/CookieConsent.jsx
+++ src/util/CookieConsent.jsx
@@ -1,44 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-export const CookieConsentContext = React.createContext(false);
+export const CookieConsentContext = React.createContext(null);
 
-function CookieConsentBanner({ onClick }) {
+function CookieConsentBanner({ accept, reject }) {
   const { t } = useTranslation();
+
+  function goToLink() {
+    window.location.href = `/#${t('App.usesCookies.url')}`;
+  }
+
   return (
     <div
       style={{ zIndex: 501 }}
-      className="fixed left-0 bottom-0 w-full bg-gray-900 p-4 text-white font-exo2 flex flex-no-wrap items-center space-between flex-row"
+      className="fixed left-0 bottom-0 w-full bg-gray-900 md:pb-0 pb-4 px-4 text-white font-exo2 flex flex-wrap md:flex-no-wrap items-center space-between justify-end flex-row"
     >
-      <div className="flex-grow">{t('App.usesCookies')}</div>
-      <button
-        type="button"
-        className="bg-primary p-2 rounded ml-2 font-open-sans flex items-center justify-center"
-        onClick={onClick}
-      >
-        Okay
-      </button>
+      <div className="flex-grow mt-4 md:my-4">
+        {t('App.usesCookies.preLink')}
+        <button type="button" onClick={goToLink} className="text-secondary hover:underline">
+          {' '}
+          {t('App.usesCookies.link')}
+        </button>
+        {t('App.usesCookies.postLink')}
+      </div>
+      <div className="flex my-2">
+        <button
+          type="button"
+          className="bg-primary p-2 rounded font-open-sans flex items-center justify-center"
+          onClick={reject}
+        >
+          {t('App.rejectCookies')}
+        </button>
+        <button
+          type="button"
+          className="bg-secondary p-2 rounded ml-2 font-open-sans flex items-center justify-center"
+          onClick={accept}
+        >
+          {t('App.acceptCookies')}
+        </button>
+      </div>
     </div>
   );
 }
 
 export default function CookieConsent({ children }) {
-  const [cookiesConsented, setCookiesConsented] = useState(false);
+  const [cookiesConsented, setCookiesConsented] = useState(null);
 
   const acceptCookies = () => {
     window.localStorage.setItem('qh-cookies-accepted', 'true');
     setCookiesConsented(true);
   };
+  const rejectCookies = () => {
+    window.localStorage.setItem('qh-cookies-accepted', 'false');
+    setCookiesConsented(false);
+  };
 
   useEffect(() => {
     const value = window.localStorage.getItem('qh-cookies-accepted');
-    const isAccepted = value === 'true';
+    const isAccepted = typeof value === 'string' ? value === 'true' : null;
     setCookiesConsented(isAccepted);
   }, []);
 
   return (
     <CookieConsentContext.Provider value={cookiesConsented}>
-      {!cookiesConsented && <CookieConsentBanner onClick={acceptCookies} />}
+      {cookiesConsented === null && <CookieConsentBanner accept={acceptCookies} reject={rejectCookies} />}
       {children}
     </CookieConsentContext.Provider>
   );
