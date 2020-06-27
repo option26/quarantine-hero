a9ad7920cd5b79c03e8c9788c66d29f3e6ccf3b4
jagaSto <gassner@stocard.de>
2020-06-27 19:15:09 +0200

    add sendEmailToUser

---
+++ firebase/functions/src/utilities/email/sendEmailToUser.ts
@@ -0,0 +1,20 @@
+import * as admin from 'firebase-admin';
+import * as sgMail from '@sendgrid/mail';
+
+import { UserRecord } from 'firebase-functions/lib/providers/auth';
+import { SendgridTemplateData } from '@interface/email/SendgridTemplateData';
+
+export async function sendEmailToUser(uid: string, templateId: string, templateData: SendgridTemplateData): Promise<string> {
+  const offeringUser = await admin.auth().getUser(uid);
+  const { email } = offeringUser.toJSON() as UserRecord;
+  const sendgridOptions = {
+    to: email,
+    from: 'help@quarantaenehelden.org',
+    templateId,
+    dynamic_template_data: templateData,
+    hideWarnings: true, // removes triple bracket warning
+  };
+  // without "any" casting, sendgrid complains about sendgridOptions typing
+  await sgMail.send(sendgridOptions as any);
+  return email as string;
+}
