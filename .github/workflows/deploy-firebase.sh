621c6909604de1b88899d61a488f75dbe3db0fc0
kenodressel <hire@keno.digital>
2020-06-27 23:20:59 +0200

    adds firebase rules to repo and deploy script (fixes #287)

--- .github/workflows/deploy-firebase.sh
+++ .github/workflows/deploy-firebase.sh
@@ -12,3 +12,4 @@ FIREBASE_PROJECT="quarantine-hero"
 FIREBASE_MESSAGE="QH $GIT_REV deployment to firebase"
 # deploy here because npm does not resolve environment variables in npm run
 npx firebase-tools deploy --only functions --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
+npx firebase-tools deploy --only firestore:rules --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
