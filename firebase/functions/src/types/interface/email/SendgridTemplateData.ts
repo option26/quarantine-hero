87e56ea9bc86e88e2412b65b611162f18ec46f3b
jagaSto <gassner@stocard.de>
2020-06-27 19:17:34 +0200

    add SendgridTemplateData

---
+++ firebase/functions/src/types/interface/email/SendgridTemplateData.ts
@@ -0,0 +1,12 @@
+import { AskForHelpCollectionEntry } from '@interface/collections/AskForHelpCollectionEntry';
+
+export interface SendgridTemplateData {
+  subject: string;
+  request: AskForHelpCollectionEntry['d']['request'];
+  location: AskForHelpCollectionEntry['d']['location'];
+  link: string;
+  reportLink?: string;
+  solveLink?: string;
+  requestMoreHelpLink?: string;
+  deleteLink?: string;
+}
