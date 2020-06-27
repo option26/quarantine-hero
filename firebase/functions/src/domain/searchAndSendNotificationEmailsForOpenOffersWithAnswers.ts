e05e8db9ef03751be7328c4f49f2481493c0fc53
jagaSto <gassner@stocard.de>
2020-06-27 19:20:04 +0200

    add searchAndSendNotificationEmailsForOpenOffersWithAnswers

---
+++ firebase/functions/src/domain/searchAndSendNotificationEmailsForOpenOffersWithAnswers.ts
@@ -0,0 +1,60 @@
+import * as admin from 'firebase-admin';
+
+import {
+  MINIMUM_NOTIFICATION_DELAY_FURTHER_CONTACT_WITH_OPEN_ENTRIES_DAYS,
+  MAXIMUM_ALLOWED_AGE_FOR_FURTHER_CONTACT_DAYS,
+  SEND_EMAILS,
+  sendingMailsDisabledLogMessage,
+} from '@config';
+
+import { sendEmailToUser } from '@utilities/email/sendEmailToUser';
+
+import { AskForHelpCollectionEntry } from '@interface/collections/AskForHelpCollectionEntry';
+import { SendgridTemplateData } from '@interface/email/SendgridTemplateData';
+import { CollectionName } from '@enum/CollectionName';
+
+export async function searchAndSendNotificationEmailsForOpenOffersWithAnswers(): Promise<void> {
+  try {
+    const db = admin.firestore();
+    const openAskForHelpSnapsWithAnswers = await db.collection(CollectionName.AskForHelp)
+      .where('d.timestamp', '<=', Date.now() - MAXIMUM_ALLOWED_AGE_FOR_FURTHER_CONTACT_DAYS * 24 * 60 * 60 * 1000)
+      .where('d.timeStamp', '<=', Date.now() - MINIMUM_NOTIFICATION_DELAY_FURTHER_CONTACT_WITH_OPEN_ENTRIES_DAYS * 24 * 60 * 60 * 1000)
+      .where('d.responses', '>', 0)
+      .limit(3)
+      .get();
+
+    // eslint-disable-next-line no-console
+    console.log('openAskForHelpSnapsWithAnswers: Requests to execute', openAskForHelpSnapsWithAnswers.docs.length);
+    // RUN SYNC
+    const asyncOperations = openAskForHelpSnapsWithAnswers.docs.map(async (askForHelpSnap) => {
+      const askForHelpSnapData = askForHelpSnap.data() as AskForHelpCollectionEntry;
+      const askForHelpId = askForHelpSnap.id;
+      // eslint-disable-next-line no-console
+      console.log('askForHelpId', askForHelpId);
+      // eslint-disable-next-line no-console
+      if (SEND_EMAILS) {
+        const templateId = 'TODO: create new template';
+        const templateData: SendgridTemplateData = {
+          subject: 'QuarantäneHeld*innen - Wurde Dir geholfen?',
+          request: askForHelpSnapData.d.request,
+          location: askForHelpSnapData.d.location,
+          link: `https://www.quarantaenehelden.org/#/offer-help/${askForHelpId}`,
+          solveLink: `https://www.quarantaenehelden.org/#/offer-help/${askForHelpId}?solve`,
+          // TODO: need UI feature for this
+          requestMoreHelpLink: `https://www.quarantaenehelden.org/#/offer-help/${askForHelpId}?more-help`,
+          deleteLink: `https://www.quarantaenehelden.org/#/offer-help/${askForHelpId}?deleteLink`,
+        };
+        const email = await sendEmailToUser(askForHelpSnapData.d.uid, templateId, templateData);
+        return email;
+      }
+      // eslint-disable-next-line no-console
+      console.log(sendingMailsDisabledLogMessage);
+      return null;
+    });
+    const result = await Promise.all(asyncOperations);
+    console.log('result', result);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+}
