d4667a2bd83b9b9fcd4ecd796aae8c2b9ac2afcb
jagaSto <gassner@stocard.de>
2020-09-27 21:00:42 +0200

    add setAnalytics, call on constructor

--- src/firebase.js
+++ src/firebase.js
@@ -11,10 +11,18 @@ class Firebase {
     app.initializeApp(config);
     this.app = app;
     this.auth = app.auth();
-    this.analytics = {
-      logEvent: () => {},
-    };
+    this.setAnalytics(false);
     this.store = app.firestore();
+  }
+
+  setAnalytics(state) {
+    if (state) {
+      this.analytics = app.analytics();
+      return;
+    }
+    this.analytics = {
+      logEvent: () => { },
+    };
   }
 }
 export default new Firebase();
