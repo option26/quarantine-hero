afff28f0e30760296ebc89dc7c4915235bdd002b
Maurice <m.ackel@icloud.com>
2020-10-13 01:47:55 +0200

    Enhanced Slack integration (#362)
    * Using postMessage API instead of webhook now. Bot now also informs about how many emails were sent and when a post gets reported.
    
    * Fixed linting errors
    
    * Added error logging on slack interaction

--- firebase/functions/src/domain/onAskForHelpCreate.ts
+++ firebase/functions/src/domain/onAskForHelpCreate.ts
@@ -13,16 +13,23 @@ export async function onAskForHelpCreate(snap: admin.firestore.DocumentSnapshot)
     const askForHelpSnap = await db.collection(parentPath).doc(askForHelpId).get();
     const askForHelpSnapData = askForHelpSnap.data() as AskForHelpCollectionEntry;
 
+    let messageRef = '';
+    try {
+      //Don't let message sending break everything
+      messageRef = await postToSlack(askForHelpId, askForHelpSnapData);
+    } catch (err) {
+      console.log('Error posting to slack', err);
+    }
+
     // Enforce field to 0
     await snap.ref.update({
       'd.notificationCounter': 0,
+      'd.slackMessageRef': messageRef,
     });
 
     await db.collection(CollectionName.Stats).doc('external').update({
       askForHelp: admin.firestore.FieldValue.increment(1),
     });
-
-    await postToSlack(askForHelpId, askForHelpSnapData);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error(e);
--- firebase/functions/src/utilities/slack.ts
+++ firebase/functions/src/utilities/slack.ts
@@ -2,19 +2,45 @@ import axios from 'axios';
 import * as functions from 'firebase-functions';
 import { AskForHelpCollectionEntry } from '../types/interface/collections/AskForHelpCollectionEntry';
 
-async function postToSlack(snapId: string, snapData: AskForHelpCollectionEntry): Promise<void> {
+async function postToSlack(snapId: string, snapData: AskForHelpCollectionEntry): Promise<string> {
   const { request, plz, location } = snapData.d;
+
+  const response = await axios({
+    method: 'POST',
+    url: 'https://slack.com/api/chat.postMessage',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': `Bearer ${functions.config().slack.token}`,
+    },
+    data: {
+      channel: functions.config().slack.channel,
+      text: `https://www.quarantaenehelden.org/#/offer-help/${snapId}\n${plz} - ${location}\n>${request.replace('\n', '\n>')}`,
+    },
+  });
+
+  return response.data.ts;
+}
+
+async function postReplyToSlack(messageRef: string | undefined, message: string, mention = false): Promise<void> {
+  if (!messageRef) {
+    return;
+  }
+
+  const text = `${mention ? `<!subteam^${functions.config().slack.group}> ` : ''}${message}`;
 
   await axios({
     method: 'POST',
-    url: functions.config().slack.url,
+    url: 'https://slack.com/api/chat.postMessage',
     headers: {
       'Content-type': 'application/json',
+      'Authorization': `Bearer ${functions.config().slack.token}`,
     },
     data: {
-      text: `https://www.quarantaenehelden.org/#/offer-help/${snapId}\n${plz} - ${location}\n>${request.replace('\n', '\n>')}`,
+      channel: functions.config().slack.channel,
+      thread_ts: messageRef,
+      text,
     },
   });
 }
 
-export { postToSlack };
+export { postToSlack, postReplyToSlack };
