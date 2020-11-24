2b4ec7f147a62c1ce62f13990cea608b5e8c533d
Maurice <m.ackel@icloud.com>
2020-11-24 15:36:32 +0100

    Housekeeping (#366)
    * Adds more translations and makes Google Maps key an environment variable
    
    * Fixes an issue that happens if there are no reported posts by a user
    
    * Updated privacy policy
    
    * Updated maintainers
    
    * Updated security readme

--- firebase/functions/src/domain/onUserDelete.ts
+++ firebase/functions/src/domain/onUserDelete.ts
@@ -45,16 +45,18 @@ export async function onUserDelete(user: UserRecord): Promise<void> {
       db.collection(CollectionName.Deleted).doc(id),
     ]).reduce((arr, elem) => arr.concat(elem), []);
 
-    const reportedEntries = await db.getAll(...entryRefs);
-    reportedEntries.forEach((doc) => {
-      if (!doc.exists) return;
-
-      promises.push(doc.ref.update({ 'd.reportedBy': admin.firestore.FieldValue.arrayRemove(user.uid) }));
-      const data = doc && doc.data();
-      if (data && !data.d.reportedBy.includes('ghost-user')) {
-        promises.push(doc.ref.update({ 'd.reportedBy': admin.firestore.FieldValue.arrayUnion('ghost-user') }));
-      }
-    });
+    if(entryRefs.length > 0) {
+      const reportedEntries = await db.getAll(...entryRefs);
+      reportedEntries.forEach((doc) => {
+        if (!doc.exists) return;
+  
+        promises.push(doc.ref.update({ 'd.reportedBy': admin.firestore.FieldValue.arrayRemove(user.uid) }));
+        const data = doc && doc.data();
+        if (data && !data.d.reportedBy.includes('ghost-user')) {
+          promises.push(doc.ref.update({ 'd.reportedBy': admin.firestore.FieldValue.arrayUnion('ghost-user') }));
+        }
+      });
+    }
 
     await Promise.all(promises);
   } catch (e) {
