e09197e5be2f7bff930caeefeae931d6a1b6dffa
Maurice <m.ackel@icloud.com>
2021-01-04 11:01:59 +0100

    Fixed linting error

--- src/util/useQuery.js
+++ src/util/useQuery.js
@@ -5,7 +5,7 @@ export default () => {
   const { search } = useLocation();
 
   return React.useMemo(() => {
-    const paramArray = [...new URLSearchParams(search).entries(), ...new URLSearchParams(window.location.search).entries()]
+    const paramArray = [...new URLSearchParams(search).entries(), ...new URLSearchParams(window.location.search).entries()];
     const paramObject = paramArray.reduce((obj, entry) => {
       const [key, value] = entry;
       obj[key] = value; // eslint-disable-line no-param-reassign
