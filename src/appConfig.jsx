d3667a22ac095cbbd464e44235bec5b9b56d0777
jagaSto <gassner@stocard.de>
2021-01-10 01:29:09 +0100

    add maxAllowedRequestForHelp, minFollowupDelayDays

--- src/appConfig.jsx
+++ src/appConfig.jsx
@@ -1,4 +1,5 @@
 const baseUrl = process.env.REACT_APP_BASE_URL;
-
+const maxAllowedRequestForHelp = Number.parseInt(process.env.REACT_APP_MAXIMUM_ALLOWED_REQUESTS_FOR_HELP, 10);
+const minFollowupDelayDays = Number.parseInt(process.env.REACT_APP_MINIMUM_FOLLOWUP_DELAY_DAYS, 10);
 // eslint-disable-next-line import/prefer-default-export
-export { baseUrl };
+export { baseUrl, maxAllowedRequestForHelp, minFollowupDelayDays };
