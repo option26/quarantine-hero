c9edf1652018c74b71042a4560e841cde39524f3
jagaSto <gassner@stocard.de>
2021-01-12 22:23:17 +0100

    rename moreHelpRequestCooldownDays

--- src/appConfig.jsx
+++ src/appConfig.jsx
@@ -1,5 +1,5 @@
 const baseUrl = process.env.REACT_APP_BASE_URL;
 const maxAllowedRequestForHelp = Number.parseInt(process.env.REACT_APP_MAXIMUM_ALLOWED_REQUESTS_FOR_HELP, 10);
-const minFollowupDelayDays = Number.parseInt(process.env.REACT_APP_MINIMUM_FOLLOWUP_DELAY_DAYS, 10);
+const moreHelpRequestCooldownDays = Number.parseInt(process.env.REACT_APP_MORE_HELP_REQUEST_COOLDOWN_DAYS, 10);
 // eslint-disable-next-line import/prefer-default-export
-export { baseUrl, maxAllowedRequestForHelp, minFollowupDelayDays };
+export { baseUrl, maxAllowedRequestForHelp, moreHelpRequestCooldownDays };
