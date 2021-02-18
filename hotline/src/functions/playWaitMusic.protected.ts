c5bb78ee84e89eb7cf4965e0c511cbc22301509f
Maurice <m.ackel@icloud.com>
2021-02-18 23:52:07 +0100

    Fixed a bug where callers were not dequeued correctly when there was no agent available

--- hotline/src/functions/playWaitMusic.protected.ts
+++ hotline/src/functions/playWaitMusic.protected.ts
@@ -2,20 +2,19 @@ import '@twilio-labs/serverless-runtime-types';
 import { Context, ServerlessCallback } from '@twilio-labs/serverless-runtime-types/types';
 import { Environment } from '../types/Environment';
 
-// This whole function gets called repeatedly (i.e. as soon as it stops) until the caller is dequeued.
+// This whole function gets called repeatedly (i.e. as soon as execution is finished, it gets called again) until the caller is dequeued.
 function playWaitMusic(context: Context<Environment>, event: { QueueTime: number }, callback: ServerlessCallback) {
   const response = new Twilio.twiml.VoiceResponse();
 
   // On every execution, we get the current wait time and if it exceeds a threshold, we dequeue caller.
   const waitTime = event.QueueTime;
-  if (waitTime >= 30) {
-    // TODO: Possibly redirect to other unavailability handler
-    response.redirect(`https://${context.DOMAIN_NAME}/noAgentAvailable`);
+  if (waitTime >= 60) {
+    // Control is handed back to the handleIncomingCall function, which will invoke the 'no agent available' function
+    response.leave();
     return callback(null, response);
   }
 
   // IMPORTANT: Use a short audio here as the length of the audio is the lower limit for the waiting time of a user
-  // TODO: Use real waiting music
   response.play({ loop: 1 }, `https://${context.DOMAIN_NAME}/Waiting.wav`);
 
   return callback(null, response);
