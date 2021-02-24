eb804775547c9cec9e1c6970fccb8b3b5f033ff3
Maurice <m.ackel@icloud.com>
2021-02-24 15:29:56 +0100

    Added integration to delete a post from slack

---
+++ firebase/functions/src/domain/onDeletePost.ts
@@ -0,0 +1,35 @@
+import * as admin from 'firebase-admin';
+import { CollectionName } from '../types/enum/CollectionName';
+import { answerDirectly, postReplyToSlack } from '../utilities/slack';
+
+export async function onDeletePost(messageRef: string, responseUrl: string) {
+    const db = admin.firestore();
+    const auth = admin.auth();
+
+    try {
+        const snapshot = await db.collection(CollectionName.AskForHelp).where('d.slackMessageRef', '==', messageRef).get();
+        
+        if(snapshot.empty) {
+            throw new Error('Keine Nachrichten mit dieser Slack-Referenz');
+        }
+
+        if(snapshot.docs.length > 1) {
+            throw new Error('Mehr als eine Nachricht mit dieser Slack-Referenz');
+        }
+
+        // Get the identified post
+        const doc = snapshot.docs[0];
+        const data = doc.data();
+
+        await db.collection(CollectionName.Deleted).doc(doc.id).set({
+          collectionName: CollectionName.AskForHelp, ...doc.data(),
+        });
+
+        await postReplyToSlack(messageRef, 'Dieser Post wurde manuell gelöscht.', true);
+
+        const user = await auth.getUser(data.d.uid);
+        await answerDirectly(`Post wurde erfolgreich gelöscht. Jetzt Ersteller*in unter ${user.email} benachrichtigen.`, responseUrl);
+    } catch (err) {
+        await answerDirectly(`Fehler beim Löschen des Posts: ${err}`, responseUrl);
+    }
+}
