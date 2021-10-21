eb381344cc27a566bd9a11ddb1fabd206f7964c3
Keno Dressel <hire@keno.digital>
2021-10-21 22:28:24 +0200

    chore(app): removes service worker

--- src/index.jsx
+++ src/index.jsx
@@ -10,12 +10,6 @@ import './styles/tailwind.css';
 import './styles/index.css';
 import './styles/App.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './i18n'; // Internalization
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
