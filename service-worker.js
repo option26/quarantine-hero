45ac3ecd53ac29e27aec5b08aafa97710612695f
mauriceackel <m.ackel@icloud.com>
2023-07-26 19:42:58 +0000

    Deploying to gh-pages from @ option26/quarantine-hero@aaba968321f1d93c3b0a04ea3046df10dd06dbbf 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9187e9f007981cc3be27aa603805f971.js"
+  "/precache-manifest.ad0d04a041b64b15bae3537bee7c1e0e.js"
 );
 
 self.addEventListener('message', (event) => {
