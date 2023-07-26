56fbd7c4535222723aaf74d5af5e9e1025482db8
Keno Dressel <hire@keno.digital>
2023-07-26 20:53:25 +0200

    chore: sets fixed firebase version

--- .github/workflows/deploy-firebase.sh
+++ .github/workflows/deploy-firebase.sh
@@ -11,5 +11,5 @@ GIT_REV="$(git rev-parse HEAD)"
 FIREBASE_PROJECT="quarantine-hero"
 FIREBASE_MESSAGE="QH $GIT_REV deployment to firebase"
 # deploy here because npm does not resolve environment variables in npm run
-npx firebase-tools deploy --only functions --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
-npx firebase-tools deploy --only firestore --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
+npx firebase-tools@8 deploy --only functions --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
+npx firebase-tools@8 deploy --only firestore --project "$FIREBASE_PROJECT" -m "$FIREBASE_MESSAGE" --non-interactive
